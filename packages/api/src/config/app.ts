2c0537d81d43ab1e8eb922df1202696966cb7396
sixnames <slava.kirshman@icloud.com>
2020-05-31 15:32:14 +0300

    api build script added

--- packages/api/src/config/app.ts
+++ packages/api/src/config/app.ts
@@ -2,6 +2,7 @@ export const { NODE_ENV, DEV_ORIGIN = 'http://localhost:3000', HTTP_PORT = 4000 
 
 export const IN_PROD = NODE_ENV === 'production';
 export const IN_TEST = NODE_ENV === 'test';
+export const IN_DEV = NODE_ENV === 'development';
 
 export const DEFAULT_CITY = 'moscow';
 export const DEFAULT_LANG = 'ru';

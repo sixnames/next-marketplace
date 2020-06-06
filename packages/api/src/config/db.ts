140b4edf057ecc594a642cd3406a81f4e6d8e733
sixnames <slava.kirshman@icloud.com>
2020-06-06 20:57:06 +0300

    run serve added

--- packages/api/src/config/db.ts
+++ packages/api/src/config/db.ts
@@ -3,7 +3,8 @@ import { ConnectionOptions } from 'mongoose';
 export const {
   DB_USERNAME = 'admin',
   DB_PASSWORD = 'secret',
-  DB_HOST = 'mongo',
+  // DB_HOST = 'mongo',
+  DB_HOST = 'localhost',
   DB_PORT = 27017,
   DB_NAME = 'winenot-db',
 
@@ -11,12 +12,14 @@ export const {
   ADMIN_EMAIL = 'admin@gmail.com',
   ADMIN_PHONE = '+79998884433',
   ADMIN_PASSWORD = 'admin',
-  MONGO_URL = `mongodb://mongo:27017/app`,
+  // MONGO_URL = `mongodb://mongo:27017/app`,
 } = process.env;
 
-/*export const DB_URI = `mongodb://${DB_USERNAME}:${encodeURIComponent(
-  `${DB_PASSWORD}`,
-)}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;*/
+export const {
+  MONGO_URL = `mongodb://${DB_USERNAME}:${encodeURIComponent(
+    `${DB_PASSWORD}`,
+  )}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+} = process.env;
 
 export const DB_OPTIONS: ConnectionOptions = {
   useNewUrlParser: true,

03092e02d222e7acedaf4913c0302b8bc0609926
sixnames <slava.kirshman@icloud.com>
2020-07-02 20:59:14 +0300

    Authorization test added

---
+++ client/cypress/e2e/user/sign-in.spec.ts
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+import { ME_AS_ADMIN } from '../../../../api/src/config';
+
+const signInPath = '/sign-in';
+const fakeEmail = 'fake@gm';
+const fakePassword = 'fake';
+
+describe('Authorization', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Should have a sign in page', () => {
+    cy.getByCy(`sign-in-link`).click();
+    cy.location('pathname').should('include', '/sign-in');
+
+    // Should show sign in validation errors
+    cy.getByCy(`sign-in-email`).type(fakeEmail).blur();
+    cy.getByCy(`sign-in-password`).type(fakePassword).blur();
+    cy.getByCy(`sign-in-submit`).click();
+    cy.getByCy(`email-error`).should('have.length', 1);
+    cy.getByCy(`password-error`).should('have.length', 1);
+
+    // User should sign in
+    cy.visit(signInPath);
+    cy.getByCy(`sign-in-email`).type(ME_AS_ADMIN.email);
+    cy.getByCy(`sign-in-password`).type(ME_AS_ADMIN.password);
+    cy.getByCy(`sign-in-submit`).click();
+    cy.location('pathname').should('eq', '/');
+
+    // User should sign out
+    cy.getByCy(`user-nav-trigger`).click();
+    cy.getByCy(`user-nav-container`).should('exist');
+    cy.getByCy(`user-nav-sign-out`).click();
+    cy.location('pathname').should('eq', '/');
+  });
+});

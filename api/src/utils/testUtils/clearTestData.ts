3c5d5ae9ae6b681a21576d74a3cc065daa2fdb75
sixnames <slava.kirshman@icloud.com>
2020-07-16 19:52:15 +0300

    clearTestData.ts old code removed

--- api/src/utils/testUtils/clearTestData.ts
+++ api/src/utils/testUtils/clearTestData.ts
@@ -4,19 +4,6 @@ import createInitialData from '../createInitialData';
 export const clearTestDataHandler = async () => {
   await mongoose.connection.db.dropDatabase();
   await createInitialData();
-  /*const models = mongoose.modelNames();
-
-  for await (const model of models) {
-    if (model === 'User') {
-      await mongoose.model(model).deleteMany({
-        name: {
-          $ne: ADMIN_NAME,
-        },
-      });
-    } else {
-      await mongoose.model(model).deleteMany({});
-    }
-  }*/
 };
 
 const clearTestData = async () => {

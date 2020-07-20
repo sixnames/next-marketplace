a656588e371789dcf801230fdf5c695f726fe4d4
sixnames <slava.kirshman@icloud.com>
2020-07-20 17:39:55 +0300

    old validation removed

--- client/validation_res/optionsGroup.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-import { langStringInputSchema } from './templates';
-
-export const optionsGroupModalSchema = (defaultLang: string) =>
-  Yup.object().shape({
-    name: langStringInputSchema({ defaultLang, entityMessage: 'Название группы опций' }),
-  });

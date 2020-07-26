f4d3b25720203dc55fd8ff75ed140e5bdc6a0565
sixnames <slava.kirshman@icloud.com>
2020-07-26 19:55:23 +0300

    rubric variants validation added

--- client/components/Modal/AddAttributesGroupToRubricModal/AddAttributesGroupToRubricModal.tsx
+++ client/components/Modal/AddAttributesGroupToRubricModal/AddAttributesGroupToRubricModal.tsx
@@ -10,6 +10,7 @@ import ModalButtons from '../ModalButtons';
 import Button from '../../Buttons/Button';
 import { addAttributesGroupToRubricSchema } from '../../../validation';
 import { AddAttributesGroupToRubricValues } from '../../../routes/Rubrics/RubricAttributes';
+import useValidationSchema from '../../../hooks/useValidationSchema';
 
 interface AddAttributesGroupToRubricModalInterface {
   testId: string;
@@ -22,6 +23,10 @@ const AddAttributesGroupToRubricModal: React.FC<AddAttributesGroupToRubricModalI
   exclude,
   confirm,
 }) => {
+  const validationSchema = useValidationSchema({
+    schema: addAttributesGroupToRubricSchema,
+    messagesKeys: ['validation.attributesGroups.id', 'validation.rubrics.id'],
+  });
   const { data, loading, error } = useGetAttributesGroupsForRubricQuery({
     variables: {
       exclude,
@@ -45,7 +50,7 @@ const AddAttributesGroupToRubricModal: React.FC<AddAttributesGroupToRubricModalI
     <ModalFrame testId={testId}>
       <ModalTitle>Выберите группу атрибутов</ModalTitle>
       <Formik
-        validationSchema={addAttributesGroupToRubricSchema}
+        validationSchema={validationSchema}
         initialValues={{ attributesGroupId: null, showInCatalogueFilter: null }}
         onSubmit={({ attributesGroupId }) => {
           confirm({

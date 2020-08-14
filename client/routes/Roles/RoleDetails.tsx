713d15c46e6681c7ed57984d56447300c205cbd0
sixnames <slava.kirshman@icloud.com>
2020-08-14 12:12:23 +0300

    RoleDetails added

--- client/routes/Roles/RoleDetails.tsx
+++ client/routes/Roles/RoleDetails.tsx
@@ -1,13 +1,95 @@
 import React from 'react';
 import { RoleContentInterface } from './RolesContent';
-// import classes from './RoleDetails.module.css';
+import InnerWide from '../../components/Inner/InnerWide';
+import { Form, Formik } from 'formik';
+import FormikTranslationsInput from '../../components/FormElements/Input/FormikTranslationsInput';
+import FormikTextarea from '../../components/FormElements/Textarea/FormikTextarea';
+import FormikCheckboxLine from '../../components/FormElements/Checkbox/FormikCheckboxLine';
+import ModalButtons from '../../components/Modal/ModalButtons';
+import Button from '../../components/Buttons/Button';
+import { useLanguageContext } from '../../context/languageContext';
+import useValidationSchema from '../../hooks/useValidationSchema';
+import { updateRoleSchema } from '../../validation/roleSchema';
+import { useUpdateRoleMutation } from '../../generated/apolloComponents';
+import { GET_ALL_ROLES_QUERY, GET_ROLE_QUERY } from '../../graphql/query/roles';
+import useMutationCallbacks from '../../hooks/useMutationCallbacks';
 
-/*interface RoleDetailsInterface {
+const RoleDetails: React.FC<RoleContentInterface> = ({ role }) => {
+  const { getLanguageFieldInitialValue, getLanguageFieldInputValue } = useLanguageContext();
+  const { onCompleteCallback, onErrorCallback, showLoading } = useMutationCallbacks({});
+  const validationSchema = useValidationSchema({
+    schema: updateRoleSchema,
+    messagesKeys: ['validation.roles.id', 'validation.roles.name', 'validation.roles.description'],
+  });
 
-}*/
+  const [updateRoleMutation] = useUpdateRoleMutation({
+    onCompleted: (data) => onCompleteCallback(data.updateRole),
+    onError: onErrorCallback,
+    awaitRefetchQueries: true,
+    refetchQueries: [
+      {
+        query: GET_ROLE_QUERY,
+        variables: {
+          id: role.id,
+        },
+      },
+      {
+        query: GET_ALL_ROLES_QUERY,
+      },
+    ],
+  });
 
-const RoleDetails: React.FC<RoleContentInterface> = () => {
-  return <div>details</div>;
+  const { name, description, isStuff, id } = role;
+  return (
+    <InnerWide>
+      <Formik
+        initialValues={{
+          id,
+          name: getLanguageFieldInitialValue(name),
+          description,
+          isStuff,
+        }}
+        validationSchema={validationSchema}
+        onSubmit={({ name, description, isStuff, id }) => {
+          showLoading();
+          updateRoleMutation({
+            variables: {
+              input: {
+                id,
+                name: getLanguageFieldInputValue(name),
+                description,
+                isStuff,
+              },
+            },
+          });
+        }}
+      >
+        {() => {
+          return (
+            <Form>
+              <FormikTranslationsInput
+                label={'Введите название'}
+                name={'name'}
+                testId={'name'}
+                showInlineError
+                isRequired
+              />
+
+              <FormikTextarea name={'description'} label={'Описание'} isRequired showInlineError />
+
+              <FormikCheckboxLine label={'Является персоналом'} name={'isStuff'} />
+
+              <ModalButtons>
+                <Button type={'submit'} testId={'role-submit'}>
+                  {name ? 'Изменить' : 'Создать'}
+                </Button>
+              </ModalButtons>
+            </Form>
+          );
+        }}
+      </Formik>
+    </InnerWide>
+  );
 };
 
 export default RoleDetails;

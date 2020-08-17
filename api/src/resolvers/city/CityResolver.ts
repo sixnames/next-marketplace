c6a11a7137575d8e80496c8069d01b00d950f89f
sixnames <slava.kirshman@icloud.com>
2020-08-17 14:02:47 +0300

    CityResolver refactored for new decorators

--- api/src/resolvers/city/CityResolver.ts
+++ api/src/resolvers/city/CityResolver.ts
@@ -3,17 +3,30 @@ import { City, CityModel } from '../../entities/City';
 import { DocumentType } from '@typegoose/typegoose';
 import { ContextInterface } from '../../types/context';
 import getLangField from '../../utils/translations/getLangField';
+import { getOperationsConfigs } from '../../utils/auth/auth';
+import { AuthMethod } from '../../decorators/methodDecorators';
+import { CustomFilter } from '../../decorators/parameterDecorators';
+import { FilterQuery } from 'mongoose';
+
+const { operationConfigRead } = getOperationsConfigs(City.name);
 
 @Resolver((_for) => City)
 export class CityResolver {
   @Query((_returns) => [City])
-  async getAllCities(): Promise<City[]> {
-    return CityModel.find({});
+  @AuthMethod(operationConfigRead)
+  async getAllCities(
+    @CustomFilter(operationConfigRead) customFilter: FilterQuery<City>,
+  ): Promise<City[]> {
+    return CityModel.find(customFilter);
   }
 
   @Query((_returns) => City)
-  async getCity(@Arg('id', (_type) => ID) id: string): Promise<City> {
-    const city = await CityModel.findById(id);
+  @AuthMethod(operationConfigRead)
+  async getCity(
+    @CustomFilter(operationConfigRead) customFilter: FilterQuery<City>,
+    @Arg('id', (_type) => ID) id: string,
+  ): Promise<City> {
+    const city = await CityModel.findOne({ _id: id, ...customFilter });
     if (!city) {
       throw new Error('City not found');
     }
@@ -22,8 +35,12 @@ export class CityResolver {
   }
 
   @Query((_returns) => City)
-  async getCityBySlug(@Arg('slug', (_type) => String) slug: string): Promise<City> {
-    const city = await CityModel.findOne({ slug });
+  @AuthMethod(operationConfigRead)
+  async getCityBySlug(
+    @CustomFilter(operationConfigRead) customFilter: FilterQuery<City>,
+    @Arg('slug', (_type) => String) slug: string,
+  ): Promise<City> {
+    const city = await CityModel.findOne({ slug, ...customFilter });
     if (!city) {
       throw new Error('City not found');
     }

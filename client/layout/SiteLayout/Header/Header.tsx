a21d206a6b55e1facf3378d008fff8c2fb202956
sixnames <slava.kirshman@icloud.com>
2020-09-05 14:03:34 +0300

    Header logo added

--- client/layout/SiteLayout/Header/Header.tsx
+++ client/layout/SiteLayout/Header/Header.tsx
@@ -3,27 +3,34 @@ import { Fragment } from 'react';
 import classes from './Header.module.css';
 import StickyNav from './StickyNav';
 import HeaderTop from './HeaderTop';
-// import { useThemeContext } from '../../../context/themeContext';
-// import { useConfigContext } from '../../../context/configContext';
+import Link from '../../../components/Link/Link';
+import { useThemeContext } from '../../../context/themeContext';
+import { useConfigContext } from '../../../context/configContext';
+import { ASSETS_URL } from '../../../config';
+import useIsMobile from '../../../hooks/useIsMobile';
 
 const Header: React.FC = () => {
-  // const { logoSlug } = useThemeContext();
-  // const isMobile = useIsMobile();
+  const { logoSlug } = useThemeContext();
+  const isMobile = useIsMobile();
   const headerRef = useRef<HTMLElement | null>(null);
-  // const { languagesList, setLanguage, isCurrentLanguage } = useLanguageContext();
-  // const { getSiteConfigSingleValue } = useConfigContext();
+  const { getSiteConfigSingleValue } = useConfigContext();
 
-  // const siteLogo = getSiteConfigSingleValue(logoSlug);
-  // const siteLogoSrc = `${ASSETS_URL}${siteLogo}?format=svg`;
+  const siteLogo = getSiteConfigSingleValue(logoSlug);
+  const siteLogoSrc = `${ASSETS_URL}${siteLogo}?format=svg`;
 
-  // const configSiteName = getSiteConfigSingleValue('siteName');
-  // const configContactPhone = getSiteConfigSingleValue('contactPhone');
+  const configSiteName = getSiteConfigSingleValue('siteName');
 
   return (
     <Fragment>
       <header className={classes.frame} ref={headerRef}>
-        <HeaderTop />
-        <div />
+        {isMobile ? null : <HeaderTop />}
+        <div className={classes.middle}>
+          <div className={classes.middleSide} />
+          <Link href={'/'} className={classes.middleLogo} aria-label={'Главная страница'}>
+            <img src={siteLogoSrc} width='166' height='27' alt={configSiteName} />
+          </Link>
+          <div className={classes.middleSide} />
+        </div>
       </header>
       <StickyNav />
     </Fragment>
--- client/layout/SiteLayout/SiteLayout.tsx
+++ client/layout/SiteLayout/SiteLayout.tsx
@@ -47,7 +47,6 @@ const SiteLayoutConsumer: React.FC<SiteLayoutConsumerInterface> = ({
         <ErrorBoundary>
           <AnimateOpacity>{children}</AnimateOpacity>
         </ErrorBoundary>
-        <div style={{ width: 100, height: '300vh' }} />
       </main>
 
       <Footer />

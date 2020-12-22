71f489aaef3913d2dcea7b8a5c3cf361c2b9e741
sixnames <slava.kirshman@icloud.com>
2020-12-22 13:03:15 +0200

    Master branch renamed to main

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -15,7 +15,7 @@ interface AppInterface {
   Component: any;
   pageProps: any;
 }
-
+// Master branch renamed to main
 const App: NextPage<AppInterface> = ({ Component, pageProps }) => {
   const apolloClient = useApollo();
 

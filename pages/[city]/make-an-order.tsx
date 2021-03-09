61d74b69e6a0ef88969656c680184316245c243a
sixnames <slava.kirshman@icloud.com>
2021-03-09 18:45:06 +0200

    make-an-order.tsx updated for city route

--- pages/[city]/make-an-order.tsx
+++ pages/[city]/make-an-order.tsx
@@ -1,10 +1,238 @@
+import Breadcrumbs from 'components/Breadcrumbs/Breadcrumbs';
+import Button from 'components/Buttons/Button';
+import Currency from 'components/Currency/Currency';
+import FormikInput from 'components/FormElements/Input/FormikInput';
+import FormikTextarea from 'components/FormElements/Textarea/FormikTextarea';
+import Icon from 'components/Icon/Icon';
+import Inner from 'components/Inner/Inner';
+import ProductShopPrices from 'components/Product/ProductShopPrices/ProductShopPrices';
+import Title from 'components/Title/Title';
+import { useNotificationsContext } from 'context/notificationsContext';
+import { useSiteContext } from 'context/siteContext';
+import { useUserContext } from 'context/userContext';
+import { Form, Formik } from 'formik';
+import { CartProductFragment } from 'generated/apolloComponents';
+import useValidationSchema from 'hooks/useValidationSchema';
 import { getCatalogueNavRubrics, getPageInitialData } from 'lib/catalogueUtils';
+import { phoneToRaw } from 'lib/phoneUtils';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { PagePropsInterface } from 'pages/_app';
 import * as React from 'react';
 import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next';
 import SiteLayout, { SiteLayoutInterface } from 'layout/SiteLayout/SiteLayout';
 import { castDbData } from 'lib/ssrUtils';
-import MakeAnOrderRoute from 'routes/MakeAnOrderRoute/MakeAnOrderRoute';
+import CartAside from 'routes/CartRoute/CartAside';
+import classes from 'routes/MakeAnOrderRoute/MakeAnOrderRoute.module.css';
+import { makeAnOrderSchema } from 'validation/orderSchema';
+
+interface OrderRouteProductInterface {
+  cartProduct: CartProductFragment;
+}
+
+const OrderRouteProduct: React.FC<OrderRouteProductInterface> = ({ cartProduct }) => {
+  const { shopProduct, amount, formattedTotalPrice } = cartProduct;
+  if (!shopProduct) {
+    return null;
+  }
+
+  const {
+    product: { mainImage, name, itemId },
+    shop,
+    formattedOldPrice,
+    discountedPercent,
+    formattedPrice,
+  } = shopProduct;
+
+  return (
+    <div className={classes.productHolder}>
+      <div data-cy={'order-product'} className={classes.product}>
+        <div className={classes.productMainGrid}>
+          <div className={classes.productImage}>
+            <div className={classes.productImageHolder}>
+              <Image src={mainImage} alt={name} title={name} layout='fill' objectFit='contain' />
+            </div>
+          </div>
+          <div>
+            <div className={classes.productArt}>{`Артикул: ${itemId}`}</div>
+            <div className={classes.productContent}>
+              <div>
+                <div className={classes.productName}>{name}</div>
+              </div>
+
+              <div>
+                <div className={classes.productTotals}>
+                  <ProductShopPrices
+                    className={classes.productTotalsPrice}
+                    formattedPrice={formattedPrice}
+                    formattedOldPrice={formattedOldPrice}
+                    discountedPercent={discountedPercent}
+                    size={'small'}
+                  />
+                  <Icon name={'cross'} className={classes.productTotalsIcon} />
+                  <div className={classes.productTotalsAmount}>{amount}</div>
+                </div>
+                <div className={classes.productTotalPrice}>
+                  Итого <Currency value={formattedTotalPrice} />
+                </div>
+              </div>
+
+              <div>
+                <div className={classes.shop}>
+                  <div>
+                    <span>винотека: </span>
+                    {shop.name}
+                  </div>
+                  <div>{shop.address.formattedAddress}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const MakeAnOrderRoute: React.FC = () => {
+  const router = useRouter();
+  const { showErrorNotification } = useNotificationsContext();
+  const { makeAnOrder } = useSiteContext();
+  const { me } = useUserContext();
+  const { cart } = useSiteContext();
+  const validationSchema = useValidationSchema({
+    schema: makeAnOrderSchema,
+  });
+
+  const { productsCount, cartProducts } = cart;
+
+  if (cartProducts.length < 1) {
+    return (
+      <div className={classes.cart}>
+        <Breadcrumbs currentPageName={'Корзина'} />
+
+        <Inner lowTop testId={'cart'}>
+          <Title className={classes.cartTitle}>Корзина пуста</Title>
+          <div className={classes.emptyBtns}>
+            <Button
+              className={classes.emptyBtnsItem}
+              theme={'secondary'}
+              onClick={() => {
+                router.push('/').catch(() => {
+                  showErrorNotification();
+                });
+              }}
+            >
+              на главную
+            </Button>
+            <Button className={classes.emptyBtnsItem} theme={'secondary'}>
+              каталог вин
+            </Button>
+          </div>
+        </Inner>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.cart} data-cy={'order-form'}>
+      <Breadcrumbs currentPageName={'Корзина'} />
+
+      <Inner lowTop testId={'cart'}>
+        <Title className={classes.cartTitle}>
+          Корзина
+          <span>{`(${productsCount})`}</span>
+        </Title>
+        <Formik
+          enableReinitialize={true}
+          validationSchema={validationSchema}
+          initialValues={{
+            name: me ? me.name : '',
+            email: me ? me.email : '',
+            phone: me ? me.phone : '',
+            comment: '',
+          }}
+          onSubmit={(values) => {
+            makeAnOrder({
+              ...values,
+              phone: phoneToRaw(values.phone),
+            });
+          }}
+        >
+          {() => {
+            return (
+              <Form>
+                <div className={classes.order}>
+                  <div data-cy={'order-products'}>
+                    <div className={classes.form}>
+                      <div className={classes.group}>
+                        <div className={classes.groupTitle}>
+                          <div className={classes.groupTitleCounter}>1</div>
+                          Личные данные
+                        </div>
+
+                        <FormikInput
+                          testId={'order-form-name'}
+                          name={'name'}
+                          label={'Имя'}
+                          isRequired
+                        />
+                        <FormikInput
+                          testId={'order-form-phone'}
+                          name={'phone'}
+                          type={'tel'}
+                          label={'Телефон'}
+                          isRequired
+                        />
+                        <FormikInput
+                          testId={'order-form-email'}
+                          name={'email'}
+                          type={'email'}
+                          label={'E-mail'}
+                          isRequired
+                        />
+                      </div>
+
+                      <div className={classes.group}>
+                        <div className={classes.groupTitle}>
+                          <div className={classes.groupTitleCounter}>2</div>
+                          Подтверждение заказа
+                        </div>
+
+                        <div className={classes.products} data-cy={'order-products'}>
+                          {cartProducts.map((cartProduct) => {
+                            return (
+                              <OrderRouteProduct cartProduct={cartProduct} key={cartProduct._id} />
+                            );
+                          })}
+                        </div>
+
+                        <FormikTextarea
+                          label={'Ваш комментарий к заказу'}
+                          testId={'order-form-comment'}
+                          name={'comment'}
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={classes.aside}>
+                    <CartAside
+                      cart={cart}
+                      buttonText={'подтвердить заказ'}
+                      backLinkHref={'/cart'}
+                      buttonType={'submit'}
+                    />
+                  </div>
+                </div>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Inner>
+    </div>
+  );
+};
 
 interface MakeAnOrderInterface extends PagePropsInterface, SiteLayoutInterface {}
 

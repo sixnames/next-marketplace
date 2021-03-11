d90a668cd373741e6cb2b46302f80d5e401bdc14
sixnames <slava.kirshman@icloud.com>
2021-03-11 17:17:14 +0200

    roles.spec.ts updated

--- pages/[city]/cms/roles/[roleId].tsx
+++ pages/[city]/cms/roles/[roleId].tsx
@@ -1,8 +1,132 @@
+import Button from 'components/Buttons/Button';
+import DataLayout from 'components/DataLayout/DataLayout';
+import DataLayoutContentFrame from 'components/DataLayout/DataLayoutContentFrame';
+import RoleMainFields from 'components/FormTemplates/RoleMainFields';
+import Inner from 'components/Inner/Inner';
+import RequestError from 'components/RequestError/RequestError';
+import Spinner from 'components/Spinner/Spinner';
+import TabsContent from 'components/TabsContent/TabsContent';
+import { Form, Formik } from 'formik';
+import {
+  CmsRoleFragment,
+  useGetRoleQuery,
+  useUpdateRoleMutation,
+} from 'generated/apolloComponents';
+import { GET_ROLE_QUERY } from 'graphql/query/rolesQueries';
+import useMutationCallbacks from 'hooks/useMutationCallbacks';
+import useTabsConfig from 'hooks/useTabsConfig';
+import useValidationSchema from 'hooks/useValidationSchema';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import AppLayout from 'layout/AppLayout/AppLayout';
 import { NextPage } from 'next';
 import { getAppInitialData } from 'lib/ssrUtils';
-import RoleRoute from 'routes/Role/RoleRoute';
+import { NavItemInterface } from 'types/clientTypes';
+import { updateRoleSchema } from 'validation/roleSchema';
+
+interface RoleDetailsInterface {
+  role: CmsRoleFragment;
+}
+
+const RoleDetails: React.FC<RoleDetailsInterface> = ({ role }) => {
+  const { onErrorCallback, onCompleteCallback, showLoading } = useMutationCallbacks();
+  const validationSchema = useValidationSchema({
+    schema: updateRoleSchema,
+  });
+  const [updateRoleMutation] = useUpdateRoleMutation({
+    onCompleted: (data) => onCompleteCallback(data.updateRole),
+    onError: onErrorCallback,
+    awaitRefetchQueries: true,
+    refetchQueries: [
+      {
+        query: GET_ROLE_QUERY,
+        variables: {
+          _id: role._id,
+        },
+      },
+    ],
+  });
+
+  return (
+    <Inner>
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{
+          roleId: role._id,
+          nameI18n: role.nameI18n,
+          description: role.description,
+          isStuff: role.isStuff,
+        }}
+        onSubmit={(values) => {
+          showLoading();
+
+          updateRoleMutation({
+            variables: {
+              input: values,
+            },
+          }).catch((e) => console.log(e));
+        }}
+      >
+        {() => {
+          return (
+            <Form>
+              <RoleMainFields />
+
+              <Button type={'submit'} testId={'role-submit'}>
+                Сохранить
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </Inner>
+  );
+};
+
+const RoleRoute: React.FC = () => {
+  const { query } = useRouter();
+  const { roleId } = query;
+  const { generateTabsConfig } = useTabsConfig();
+  const { data, loading, error } = useGetRoleQuery({
+    variables: {
+      _id: `${roleId}`,
+    },
+  });
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error || !data || !data.getRole) {
+    return <RequestError />;
+  }
+
+  const { getRole } = data;
+
+  // Role nav tabs config
+  const navConfig: NavItemInterface[] = generateTabsConfig({
+    config: [
+      {
+        name: 'Детали',
+        testId: 'details',
+      },
+    ],
+  });
+
+  return (
+    <DataLayout
+      filterResultNavConfig={navConfig}
+      title={getRole.name}
+      filterResult={() => (
+        <DataLayoutContentFrame>
+          <TabsContent>
+            <RoleDetails role={getRole} />
+          </TabsContent>
+        </DataLayoutContentFrame>
+      )}
+    />
+  );
+};
 
 const Role: NextPage = () => {
   return (
--- pages/[city]/cms/roles/index.tsx
+++ pages/[city]/cms/roles/index.tsx
@@ -1,8 +1,153 @@
+import ContentItemControls from 'components/ContentItemControls/ContentItemControls';
+import DataLayout from 'components/DataLayout/DataLayout';
+import DataLayoutContentFrame from 'components/DataLayout/DataLayoutContentFrame';
+import DataLayoutTitle from 'components/DataLayout/DataLayoutTitle';
+import { ConfirmModalInterface } from 'components/Modal/ConfirmModal/ConfirmModal';
+import { CreateRoleModalInterface } from 'components/Modal/CreateRoleModal/CreateRoleModal';
+import RequestError from 'components/RequestError/RequestError';
+import Spinner from 'components/Spinner/Spinner';
+import Table, { TableColumn } from 'components/Table/Table';
+import { ROUTE_CMS } from 'config/common';
+import { CONFIRM_MODAL, CREATE_ROLE_MODAL } from 'config/modals';
+import {
+  CmsRoleFragment,
+  useCreateRoleMutation,
+  useDeleteRoleMutation,
+  useGetAllRolesQuery,
+} from 'generated/apolloComponents';
+import { GET_ALL_ROLES_QUERY } from 'graphql/query/rolesQueries';
+import useMutationCallbacks from 'hooks/useMutationCallbacks';
+import useSessionCity from 'hooks/useSessionCity';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import AppLayout from 'layout/AppLayout/AppLayout';
 import { NextPage } from 'next';
 import { getAppInitialData } from 'lib/ssrUtils';
-import RolesRoute from 'routes/Roles/RolesRoute';
+
+const RolesContent: React.FC = () => {
+  const city = useSessionCity();
+  const router = useRouter();
+  const { onCompleteCallback, onErrorCallback, showModal } = useMutationCallbacks({
+    withModal: true,
+  });
+  const { data, loading, error } = useGetAllRolesQuery({
+    fetchPolicy: 'network-only',
+  });
+
+  const refetchQueries = [
+    {
+      query: GET_ALL_ROLES_QUERY,
+    },
+  ];
+
+  const [deleteRoleMutation] = useDeleteRoleMutation({
+    onCompleted: (data) => onCompleteCallback(data.deleteRole),
+    onError: onErrorCallback,
+    awaitRefetchQueries: true,
+    refetchQueries,
+  });
+
+  const [createRoleMutation] = useCreateRoleMutation({
+    onCompleted: (data) => onCompleteCallback(data.createRole),
+    onError: onErrorCallback,
+    awaitRefetchQueries: true,
+    refetchQueries,
+  });
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error || !data) {
+    return <RequestError />;
+  }
+
+  const { getAllRoles } = data;
+
+  const columns: TableColumn<CmsRoleFragment>[] = [
+    {
+      headTitle: 'Название',
+      accessor: 'name',
+      render: ({ cellData }) => cellData,
+    },
+    {
+      headTitle: 'Сотрудник сайта',
+      accessor: 'isStuff',
+      render: ({ cellData }) => (cellData ? 'Да' : 'Нет'),
+    },
+    {
+      render: ({ dataItem }) => {
+        return (
+          <ContentItemControls
+            justifyContent={'flex-end'}
+            testId={dataItem.name}
+            updateTitle={'Редактировать роль'}
+            updateHandler={() => {
+              router
+                .push(`/${city}${ROUTE_CMS}/roles/${dataItem._id}`)
+                .catch((e) => console.log(e));
+            }}
+            deleteTitle={'Удалить роль'}
+            deleteHandler={() => {
+              showModal<ConfirmModalInterface>({
+                variant: CONFIRM_MODAL,
+                props: {
+                  testId: 'delete-role-modal',
+                  message:
+                    'Вы уверены, что хотите удалить роль? Всем пользователям с данной ролью будет назначена роль Гость.',
+                  confirm: () => {
+                    deleteRoleMutation({
+                      variables: { _id: dataItem._id },
+                    }).catch((e) => console.log(e));
+                  },
+                },
+              });
+            }}
+          />
+        );
+      },
+    },
+  ];
+
+  return (
+    <DataLayoutContentFrame>
+      <DataLayoutTitle
+        titleRight={
+          <ContentItemControls
+            testId={'roles'}
+            createTitle={'Добавить роль'}
+            createHandler={() => {
+              showModal<CreateRoleModalInterface>({
+                variant: CREATE_ROLE_MODAL,
+                props: {
+                  confirm: (values) => {
+                    createRoleMutation({
+                      variables: {
+                        input: values,
+                      },
+                    }).catch((e) => console.log(e));
+                  },
+                },
+              });
+            }}
+          />
+        }
+      />
+      <Table<CmsRoleFragment> columns={columns} data={getAllRoles} testIdKey={'name'} />
+    </DataLayoutContentFrame>
+  );
+};
+
+const RolesRoute: React.FC = () => {
+  return (
+    <DataLayout
+      title={'Роли'}
+      filterResult={() => {
+        return <RolesContent />;
+      }}
+    />
+  );
+};
 
 const Roles: NextPage = () => {
   return (

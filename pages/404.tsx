281c8423d2a705cf08b5b24bd5a671421ef41f75
sixnames <slava.kirshman@icloud.com>
2021-03-15 19:39:26 +0200

    404 page updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,15 @@
 import Inner from 'components/Inner/Inner';
+import Link from 'components/Link/Link';
 import Title from 'components/Title/Title';
+import { DEFAULT_CITY } from 'config/common';
 import { NextPage } from 'next';
 import * as React from 'react';
 
 const ErrorPage: NextPage = () => {
   return (
     <Inner>
-      <Title>Page not found</Title>
+      <Title>404 Стараница не найдена</Title>
+      <Link href={`/${DEFAULT_CITY}`}>На главную</Link>
     </Inner>
   );
 };

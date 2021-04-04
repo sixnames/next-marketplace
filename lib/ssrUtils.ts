a82b7898c89a3736ad9915483709f7a9a9b6d0ac
sixnames <slava.kirshman@icloud.com>
2021-04-04 12:47:11 +0300

    App sessionUser query added

--- lib/ssrUtils.ts
+++ lib/ssrUtils.ts
@@ -1,10 +1,20 @@
-import { DEFAULT_CITY, DEFAULT_LOCALE, ROLE_SLUG_ADMIN, ROUTE_SIGN_IN } from 'config/common';
-import { COL_COMPANIES, COL_ROLES, COL_USERS } from 'db/collectionNames';
-import { CityModel, CompanyModel, RoleModel, UserModel } from 'db/dbModels';
+import {
+  DEFAULT_CITY,
+  DEFAULT_LOCALE,
+  ROLE_SLUG_ADMIN,
+  ROLE_SLUG_COMPANY_MANAGER,
+  ROLE_SLUG_COMPANY_OWNER,
+  ROUTE_APP_NAV_GROUP,
+  ROUTE_CMS_NAV_GROUP,
+  ROUTE_SIGN_IN,
+} from 'config/common';
+import { COL_COMPANIES, COL_NAV_ITEMS, COL_ROLES, COL_USERS } from 'db/collectionNames';
+import { CityModel, CompanyModel, UserModel } from 'db/dbModels';
 import { getDatabase } from 'db/mongodb';
 import { SiteLayoutInterface } from 'layout/SiteLayout/SiteLayout';
 import { getCatalogueNavRubrics, getPageInitialData } from 'lib/catalogueUtils';
 import { getI18nLocaleValue } from 'lib/i18n';
+import { getFullName, getShortName } from 'lib/nameUtils';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { getSession } from 'next-auth/client';
 import { PagePropsInterface } from 'pages/_app';
@@ -39,15 +49,95 @@ export async function getAppInitialData({
   }
 
   // Session user
+  // const sessionUserStart = new Date().getTime();
   const db = await getDatabase();
-  const user = await db.collection<UserModel>(COL_USERS).findOne(
-    { email: `${session.user.email}` },
-    {
-      projection: {
-        password: false,
+  const usersCollection = db.collection<UserModel>(COL_USERS);
+  const userAggregation = await usersCollection
+    .aggregate([
+      {
+        $match: {
+          email: `${session.user.email}`,
+        },
       },
-    },
-  );
+      {
+        $lookup: {
+          from: COL_ROLES,
+          as: 'roles',
+          let: { roleId: '$roleId' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ['$_id', '$$roleId'],
+                },
+              },
+            },
+            {
+              $lookup: {
+                from: COL_NAV_ITEMS,
+                as: 'navItems',
+                let: { allowedAppNavigation: '$allowedAppNavigation' },
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $in: ['$_id', '$$allowedAppNavigation'],
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              $addFields: {
+                name: `$nameI18n.${sessionLocale}`,
+                appNavigation: {
+                  $filter: {
+                    input: '$navItems',
+                    as: 'navItem',
+                    cond: {
+                      $eq: ['$$navItem.navGroup', ROUTE_APP_NAV_GROUP],
+                    },
+                  },
+                },
+                cmsNavigation: {
+                  $filter: {
+                    input: '$navItems',
+                    as: 'navItem',
+                    cond: {
+                      $eq: ['$$navItem.navGroup', ROUTE_CMS_NAV_GROUP],
+                    },
+                  },
+                },
+              },
+            },
+            {
+              $project: {
+                nameI18n: false,
+                navItems: false,
+                createdAt: false,
+                updatedAt: false,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          role: { $arrayElemAt: ['$roles', 0] },
+        },
+      },
+      {
+        $project: {
+          password: false,
+          roles: false,
+          createdAt: false,
+          updatedAt: false,
+        },
+      },
+    ])
+    .toArray();
+  const user = userAggregation[0];
 
   if (!user) {
     return {
@@ -58,8 +148,7 @@ export async function getAppInitialData({
     };
   }
 
-  const role = await db.collection<RoleModel>(COL_ROLES).findOne({ _id: user.roleId });
-  if ((!role || role.slug !== ROLE_SLUG_ADMIN) && isCms) {
+  if (!user.role) {
     return {
       redirect: {
         permanent: false,
@@ -67,6 +156,31 @@ export async function getAppInitialData({
       },
     };
   }
+
+  if (user.role.slug !== ROLE_SLUG_ADMIN && isCms) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/`,
+      },
+    };
+  }
+
+  if (user.role.slug !== ROLE_SLUG_COMPANY_MANAGER && user.role.slug !== ROLE_SLUG_COMPANY_OWNER) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/`,
+      },
+    };
+  }
+
+  const sessionUser: UserModel = {
+    ...user,
+    fullName: getFullName(user),
+    shortName: getShortName(user),
+  };
+  // console.log('Session user ', new Date().getTime() - sessionUserStart);
 
   const initialDataProps = {
     locale: `${sessionLocale}`,
@@ -79,7 +193,7 @@ export async function getAppInitialData({
   const currentCity = initialData.cities.find(({ slug }: CityModel) => {
     return slug === sessionCity;
   });
-
+  console.log('Rendered >>');
   return {
     props: {
       initialData,
@@ -90,8 +204,8 @@ export async function getAppInitialData({
           }
         : null,
       sessionCity: currentCity ? sessionCity : DEFAULT_CITY,
+      sessionUser: castDbData(sessionUser),
       sessionLocale,
-      sessionUser: castDbData(user),
       pageUrls: {
         canonicalUrl: `https://${host}${path}`,
         siteUrl: `https://${host}`,

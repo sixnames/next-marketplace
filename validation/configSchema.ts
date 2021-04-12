812f7a4dd3be44ded4073910bfbf2e3ee41ceefe
sixnames <slava.kirshman@icloud.com>
2021-04-12 11:17:28 +0300

    configSchema validation updated

--- validation/configSchema.ts
+++ validation/configSchema.ts
@@ -1,26 +1,16 @@
 import * as Yup from 'yup';
 import { ValidationSchemaArgsInterface } from 'types/validataionTypes';
-import {
-  objectIdSchema,
-  requiredAssetSchema,
-  requiredStringSchema,
-} from 'validation/schemaTemplates';
+import { objectIdSchema, requiredAssetSchema } from 'validation/schemaTemplates';
 import { DEFAULT_CITY, DEFAULT_LOCALE } from 'config/common';
 
 export const configIdSchema = (args: ValidationSchemaArgsInterface) => {
   return objectIdSchema({ ...args, slug: 'validation.configs.id' });
 };
 
-export const configCitiesSchema = (args: ValidationSchemaArgsInterface) => {
+export const configCitiesSchema = () => {
   return Yup.object({
     [DEFAULT_CITY]: Yup.object({
-      [DEFAULT_LOCALE]: Yup.array().of(
-        requiredStringSchema({
-          ...args,
-          slug: 'validation.configs.value',
-          max: 9999999,
-        }),
-      ),
+      [DEFAULT_LOCALE]: Yup.array().of(Yup.string()),
     }),
   });
 };
@@ -28,7 +18,7 @@ export const configCitiesSchema = (args: ValidationSchemaArgsInterface) => {
 export const updateConfigSchema = (args: ValidationSchemaArgsInterface) =>
   Yup.object().shape({
     configId: configIdSchema(args),
-    cities: configCitiesSchema(args),
+    cities: configCitiesSchema(),
   });
 
 export const updateAssetConfigSchema = (args: ValidationSchemaArgsInterface) =>

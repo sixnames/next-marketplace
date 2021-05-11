74664c8cf77d473b161dc3791c2fb28eb9ac3db4
sixnames <slava.kirshman@icloud.com>
2021-05-11 13:51:27 +0300

    attributes test data added

--- cypress/fixtures/data/attributesGroups/attributesGroups.ts
+++ cypress/fixtures/data/attributesGroups/attributesGroups.ts
@@ -3,60 +3,72 @@ import { getObjectId, getObjectIds } from 'mongo-seeding';
 
 const attributesGroups: AttributesGroupModel[] = [
   {
-    _id: getObjectId('Общие характеристики'),
+    _id: getObjectId('attributesGroup Общие характеристики'),
     nameI18n: {
       ru: 'Общие характеристики',
     },
-    attributesIds: [],
+    attributesIds: getObjectIds([
+      `attribute Описание`,
+      `attribute Количество в упаковке`,
+      `attribute Крепость`,
+      `attribute Состав`,
+      `attribute Регион`,
+      `attribute Объем`,
+      `attribute Тип ёмкости`,
+      `attribute Год`,
+      `attribute Виноград`,
+      `attribute Винтаж`,
+      `attribute Сахар`,
+    ]),
   },
   {
-    _id: getObjectId('Характеристики пива'),
+    _id: getObjectId('attributesGroup Характеристики пива'),
     nameI18n: {
       ru: 'Характеристики пива',
     },
-    attributesIds: [],
+    attributesIds: getObjectIds([]),
   },
   {
-    _id: getObjectId('Характеристики шампанского'),
+    _id: getObjectId('attributesGroup Характеристики шампанского'),
     nameI18n: {
       ru: 'Характеристики шампанского',
     },
     attributesIds: [],
   },
   {
-    _id: getObjectId('Характеристики коньяка'),
+    _id: getObjectId('attributesGroup Характеристики коньяка'),
     nameI18n: {
       ru: 'Характеристики коньяка',
     },
     attributesIds: [],
   },
   {
-    _id: getObjectId('Характеристики ликёра'),
+    _id: getObjectId('attributesGroup Характеристики ликёра'),
     nameI18n: {
       ru: 'Характеристики ликёра',
     },
     attributesIds: [],
   },
   {
-    _id: getObjectId('Характеристики водки'),
+    _id: getObjectId('attributesGroup Характеристики водки'),
     nameI18n: {
       ru: 'Характеристики водки',
     },
     attributesIds: [],
   },
   {
-    _id: getObjectId('Характеристики виски'),
+    _id: getObjectId('attributesGroup Характеристики виски'),
     nameI18n: {
       ru: 'Характеристики виски',
     },
     attributesIds: [],
   },
   {
-    _id: getObjectId('Характеристики вина'),
+    _id: getObjectId('attributesGroup Характеристики вина'),
     nameI18n: {
       ru: 'Характеристики вина',
     },
-    attributesIds: [],
+    attributesIds: getObjectIds([`attribute Тип вина`]),
   },
 ];
 

09be88e96cd38d1c971a3c7dced0b367de6f3d72
sixnames <slava.kirshman@icloud.com>
2021-05-11 11:41:30 +0300

    admin user test data added

--- cypress/fixtures/data/users/users.ts
+++ cypress/fixtures/data/users/users.ts
@@ -1,11 +1,20 @@
-// import { UserModel } from '../../../../db/dbModels';
+import { UserModel } from '../../../../db/dbModels';
 import { getObjectId } from 'mongo-seeding';
 
 const adminName = 'admin';
+const password = '$2b$10$u7zEl9.8vpdshO9tbBIHXeM5HrhLQBCEJ6mcQVSSP0B.WCb9rVE.K';
 
-const users = [
+const users: UserModel[] = [
   {
     _id: getObjectId(adminName),
+    itemId: '000001',
+    lastName: 'Site',
+    secondName: 'Second',
+    email: 'admin@gmial.com',
+    phone: '+78889990011',
+    password,
+    avatar: null,
+    cartId: null,
     name: adminName,
     roleId: getObjectId('adminRole'),
     createdAt: new Date(),

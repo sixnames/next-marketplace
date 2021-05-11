d46264c70a4865d731732ad69f815220b7ac5866
sixnames <slava.kirshman@icloud.com>
2021-05-11 19:51:27 +0300

    rubricVariants test data added

---
+++ cypress/fixtures/data/rubricVariants/rubricVariants.ts
@@ -0,0 +1,13 @@
+import { RubricVariantModel } from '../../../../db/dbModels';
+import { getObjectId } from 'mongo-seeding';
+
+const rubricVariants: RubricVariantModel[] = [
+  {
+    _id: getObjectId('rubricVariant alcohol'),
+    nameI18n: {
+      ru: 'Алкоголь',
+    },
+  },
+];
+
+export = rubricVariants;

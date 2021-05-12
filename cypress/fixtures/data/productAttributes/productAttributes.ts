8fdeb1a64bb465adec60314108714c0190c65227
sixnames <slava.kirshman@icloud.com>
2021-05-12 14:10:37 +0300

    productAttributes test data added

---
+++ cypress/fixtures/data/productAttributes/productAttributes.ts
@@ -0,0 +1,84 @@
+import {
+  ATTRIBUTE_VARIANT_NUMBER,
+  ATTRIBUTE_VARIANT_STRING,
+  CATALOGUE_OPTION_SEPARATOR,
+} from '../../../../config/common';
+import { ObjectIdModel, ProductAttributeModel } from '../../../../db/dbModels';
+import { getObjectId } from 'mongo-seeding';
+import * as rubricAttributes from '../rubricAttributes/rubricAttributes';
+import * as products from '../products/products';
+import * as options from '../options/options';
+
+const attributeText = `
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi debitis eligendi eum, excepturi iure libero molestias quas quis ratione reiciendis sed sequi sint sit! Architecto minus modi officia provident voluptates.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi debitis eligendi eum, excepturi iure libero molestias quas quis ratione reiciendis sed sequi sint sit! Architecto minus modi officia provident voluptates.
+`;
+
+const productAttributes: ProductAttributeModel[] = products.reduce(
+  (acc: ProductAttributeModel[], product) => {
+    const currentProductInitialAttributes = rubricAttributes.filter((rubricAttribute) => {
+      return product.rubricSlug === rubricAttribute.rubricSlug;
+    });
+
+    const currentProductAttributes: ProductAttributeModel[] = currentProductInitialAttributes.map(
+      (rubricAttribute) => {
+        const rubricAttributeOptions = options.filter((option) => {
+          return rubricAttribute.optionsGroupId?.equals(option.optionsGroupId);
+        });
+
+        const cleanOptionSlugs = product.selectedOptionsSlugs.reduce(
+          (acc: string[], initialSlug) => {
+            const slugArray = initialSlug.split(CATALOGUE_OPTION_SEPARATOR);
+            const cleanSlug = slugArray[1];
+            if (cleanSlug) {
+              return [...acc, cleanSlug];
+            }
+            return acc;
+          },
+          [],
+        );
+
+        const selectedOptionsIds = rubricAttributeOptions.reduce((acc: ObjectIdModel[], option) => {
+          if (cleanOptionSlugs.includes(option.slug)) {
+            return [...acc, option._id];
+          }
+          return acc;
+        }, []);
+
+        return {
+          _id: getObjectId(`${product.slug} ${rubricAttribute.slug}`),
+          productSlug: product.slug,
+          productId: product._id,
+          attributeId: rubricAttribute._id,
+          selectedOptionsSlugs: product.selectedOptionsSlugs,
+          selectedOptionsIds,
+          optionsGroupId: rubricAttribute.optionsGroupId,
+          slug: rubricAttribute.slug,
+          variant: rubricAttribute.variant,
+          attributesGroupId: rubricAttribute.attributesGroupId,
+          viewVariant: rubricAttribute.viewVariant,
+          nameI18n: rubricAttribute.nameI18n,
+          capitalise: rubricAttribute.capitalise,
+          metric: rubricAttribute.metric,
+          notShowAsAlphabet: rubricAttribute.notShowAsAlphabet,
+          positioningInTitle: rubricAttribute.positioningInTitle,
+          showInCard: true,
+          showAsBreadcrumb: true,
+          textI18n:
+            rubricAttribute.variant === ATTRIBUTE_VARIANT_STRING
+              ? {
+                  ru: attributeText,
+                }
+              : undefined,
+          number:
+            rubricAttribute.variant === ATTRIBUTE_VARIANT_NUMBER ? Math.random() * 10 : undefined,
+        };
+      },
+    );
+
+    return [...acc, ...currentProductAttributes];
+  },
+  [],
+);
+
+export = productAttributes;

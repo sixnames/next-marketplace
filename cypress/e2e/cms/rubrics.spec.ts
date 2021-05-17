dad6a00e663d882a161e5c845cc515f2b2dda22f
sixnames <slava.kirshman@icloud.com>
2021-05-17 19:43:44 +0300

    rubrics list tests restored

--- cypress/e2e/cms/rubrics.spec.ts
+++ cypress/e2e/cms/rubrics.spec.ts
@@ -1,4 +1,12 @@
-import { ADULT_KEY, ADULT_TRUE, ROUTE_CMS } from 'config/common';
+import {
+  ADULT_KEY,
+  ADULT_TRUE,
+  DEFAULT_LOCALE,
+  GENDER_HE,
+  GENDER_SHE,
+  ROUTE_CMS,
+  SECONDARY_LOCALE,
+} from 'config/common';
 
 describe('Rubrics', () => {
   beforeEach(() => {
@@ -8,76 +16,63 @@ describe('Rubrics', () => {
   });
 
   it('Should CRUD rubrics', () => {
-    cy.visit('/');
-    // const mockRubricAName = mockData.rubricADefaultName;
-    // const mockRubricVariantName = mockData.rubricVariantAlcohol.nameI18n[DEFAULT_LOCALE];
-    // const mockNewRubricA = 'mockNewRubricA';
-    // const mockNewRubricB = 'mockNewRubricB';
-
-    // Should show validation errors on new rubric creation
-    // cy.getByCy(`rubrics-create`).click();
-    // cy.getByCy(`create-rubric-modal`).should('exist');
-    // cy.getByCy(`rubric-submit`).click();
-    // cy.getByCy(`nameI18n.${DEFAULT_LOCALE}-error`).should('exist');
-    // cy.getByCy(`descriptionI18n.${DEFAULT_LOCALE}-error`).should('exist');
-    // cy.getByCy(`shortDescriptionI18n.${DEFAULT_LOCALE}-error`).should('exist');
-    // cy.getByCy(`catalogueTitle.defaultTitleI18n.${DEFAULT_LOCALE}-error`).should('exist');
-    // cy.getByCy(`catalogueTitle.keywordI18n.${DEFAULT_LOCALE}-error`).should('exist');
-    // cy.getByCy(`catalogueTitle.gender-error`).should('exist');
-    // cy.getByCy(`variantId-error`).should('exist');
+    const mainRubricName = 'Вино';
+    const rubricVariantName = 'Алкоголь';
+    const newRubricName = 'newRubricName';
+    const updatedRubricName = 'updatedRubricName';
 
     // Shouldn't create a new rubric if exists
-    // cy.getByCy(`nameI18n-${DEFAULT_LOCALE}`).type(mockRubricAName);
-    // cy.getByCy(`descriptionI18n-${DEFAULT_LOCALE}`).type(mockRubricAName);
-    // cy.getByCy(`shortDescriptionI18n-${DEFAULT_LOCALE}`).type(mockRubricAName);
-    // cy.getByCy(`catalogueTitle-defaultTitleI18n-${DEFAULT_LOCALE}`).type(mockRubricAName);
-    // cy.getByCy(`catalogueTitle-prefixI18n-${DEFAULT_LOCALE}`).type(mockRubricAName);
-    // cy.getByCy(`catalogueTitle-keywordI18n-${DEFAULT_LOCALE}`).type(mockRubricAName);
-    // cy.selectOptionByTestId(`variantId`, mockRubricVariantName);
-    // cy.getByCy(`catalogueTitle-gender`).select(GENDER_SHE);
-    // cy.getByCy(`rubric-submit`).click();
-    // cy.getByCy(`create-rubric-modal`).should('not.exist');
-    // cy.shouldError();
+    cy.getByCy(`create-rubric`).click();
+    cy.getByCy(`create-rubric-modal`).should('exist');
+    cy.getByCy(`nameI18n-${DEFAULT_LOCALE}`).type(mainRubricName);
+    cy.getByCy(`descriptionI18n-${DEFAULT_LOCALE}`).type(mainRubricName);
+    cy.getByCy(`shortDescriptionI18n-${DEFAULT_LOCALE}`).type(mainRubricName);
+    cy.getByCy(`catalogueTitle-defaultTitleI18n-${DEFAULT_LOCALE}`).type(mainRubricName);
+    cy.getByCy(`catalogueTitle-prefixI18n-${DEFAULT_LOCALE}`).type(mainRubricName);
+    cy.getByCy(`catalogueTitle-keywordI18n-${DEFAULT_LOCALE}`).type(mainRubricName);
+    cy.selectOptionByTestId(`variantId`, rubricVariantName);
+    cy.getByCy(`catalogueTitle-gender`).select(GENDER_SHE);
+    cy.getByCy(`rubric-submit`).click();
+    cy.getByCy(`create-rubric-modal`).should('not.exist');
+    cy.shouldError();
 
     // Should create new rubrics
-    // cy.getByCy(`rubrics-create`).click();
-    // cy.getByCy(`nameI18n-${DEFAULT_LOCALE}`).type(mockNewRubricA);
-    // cy.getByCy(`descriptionI18n-${DEFAULT_LOCALE}`).type(mockNewRubricA);
-    // cy.getByCy(`shortDescriptionI18n-${DEFAULT_LOCALE}`).type(mockNewRubricA);
-    // cy.getByCy(`catalogueTitle-defaultTitleI18n-${DEFAULT_LOCALE}`).type(mockNewRubricA);
-    // cy.getByCy(`catalogueTitle-prefixI18n-${DEFAULT_LOCALE}`).type(mockNewRubricA);
-    // cy.getByCy(`catalogueTitle-keywordI18n-${DEFAULT_LOCALE}`).type(mockNewRubricA);
-    // cy.selectOptionByTestId(`variantId`, mockRubricVariantName);
-    // cy.getByCy(`catalogueTitle-gender`).select(GENDER_SHE);
-    // cy.getByCy(`rubric-submit`).click();
-    // cy.shouldSuccess();
-    // cy.getByCy(`${mockNewRubricA}-row`).should('exist');
+    cy.getByCy(`create-rubric`).click();
+    cy.getByCy(`nameI18n-${DEFAULT_LOCALE}`).type(newRubricName);
+    cy.getByCy(`descriptionI18n-${DEFAULT_LOCALE}`).type(newRubricName);
+    cy.getByCy(`shortDescriptionI18n-${DEFAULT_LOCALE}`).type(newRubricName);
+    cy.getByCy(`catalogueTitle-defaultTitleI18n-${DEFAULT_LOCALE}`).type(newRubricName);
+    cy.getByCy(`catalogueTitle-prefixI18n-${DEFAULT_LOCALE}`).type(newRubricName);
+    cy.getByCy(`catalogueTitle-keywordI18n-${DEFAULT_LOCALE}`).type(newRubricName);
+    cy.selectOptionByTestId(`variantId`, rubricVariantName);
+    cy.getByCy(`catalogueTitle-gender`).select(GENDER_SHE);
+    cy.getByCy(`rubric-submit`).click();
+    cy.wait(1500);
+    cy.getByCy(`${newRubricName}-row`).should('exist');
 
     // Should delete rubric
-    // cy.getByCy(`${mockNewRubricA}-delete`).click();
-    // cy.getByCy('delete-rubric-modal').should('exist');
-    // cy.getByCy('confirm').click();
-    // cy.shouldSuccess();
-    // cy.getByCy(`${mockNewRubricA}-row`).should('not.exist');
-    //
+    cy.getByCy(`${newRubricName}-delete`).click();
+    cy.getByCy('delete-rubric-modal').should('exist');
+    cy.getByCy('confirm').click();
+    cy.wait(1500);
+    cy.getByCy(`${newRubricName}-row`).should('not.exist');
+
     // Should have rubric details tab and should update rubric
-    // cy.getByCy(`${mockRubricAName}-update`).click();
-    // cy.getByCy(`nameI18n-accordion-${SECONDARY_LOCALE}`).click();
-
-    // cy.getByCy(`nameI18n-${DEFAULT_LOCALE}`).clear().type(mockNewRubricB);
-    // cy.getByCy(`nameI18n-${SECONDARY_LOCALE}`).clear().type(mockNewRubricB);
-
-    // cy.getByCy(`descriptionI18n-${DEFAULT_LOCALE}`).clear().type(mockNewRubricB);
-    // cy.getByCy(`shortDescriptionI18n-${DEFAULT_LOCALE}`).clear().type(mockNewRubricB);
-    // cy.getByCy(`catalogueTitle-defaultTitleI18n-${DEFAULT_LOCALE}`).clear().type(mockNewRubricB);
-    // cy.getByCy(`catalogueTitle-prefixI18n-${DEFAULT_LOCALE}`).clear().type(mockNewRubricB);
-    // cy.getByCy(`catalogueTitle-keywordI18n-${DEFAULT_LOCALE}`).clear().type(mockNewRubricB);
-    // cy.selectOptionByTestId(`variantId`, mockRubricVariantName);
-    // cy.getByCy(`catalogueTitle-gender`).select(GENDER_HE);
-
-    // cy.getByCy('rubric-submit').click();
-    // cy.shouldSuccess();
-    // cy.visit(`${DEFAULT_CITY}${ROUTE_CMS}/rubrics`);
-    // cy.getByCy(`${mockNewRubricB}-row`).should('exist');
+    cy.getByCy(`${mainRubricName}-update`).click();
+    cy.getByCy(`details`).click();
+    cy.getByCy(`nameI18n-accordion-${SECONDARY_LOCALE}`).click();
+    cy.getByCy(`nameI18n-${DEFAULT_LOCALE}`).clear().type(updatedRubricName);
+    cy.getByCy(`nameI18n-${SECONDARY_LOCALE}`).clear().type(updatedRubricName);
+    cy.getByCy(`descriptionI18n-${DEFAULT_LOCALE}`).clear().type(updatedRubricName);
+    cy.getByCy(`shortDescriptionI18n-${DEFAULT_LOCALE}`).clear().type(updatedRubricName);
+    cy.getByCy(`catalogueTitle-defaultTitleI18n-${DEFAULT_LOCALE}`).clear().type(updatedRubricName);
+    cy.getByCy(`catalogueTitle-prefixI18n-${DEFAULT_LOCALE}`).clear().type(updatedRubricName);
+    cy.getByCy(`catalogueTitle-keywordI18n-${DEFAULT_LOCALE}`).clear().type(updatedRubricName);
+    cy.selectOptionByTestId(`variantId`, rubricVariantName);
+    cy.getByCy(`catalogueTitle-gender`).select(GENDER_HE);
+    cy.getByCy('rubric-submit').click();
+    cy.wait(1500);
+    cy.visit(`${ROUTE_CMS}/rubrics`);
+    cy.getByCy(`${updatedRubricName}-row`).should('exist');
   });
 });

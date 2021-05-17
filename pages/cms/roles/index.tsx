26261eeca1157de3f451274694f1fd49bb8569b3
sixnames <slava.kirshman@icloud.com>
2021-05-17 17:56:50 +0300

    cms roles ui updated

--- pages/cms/roles/index.tsx
+++ pages/cms/roles/index.tsx
@@ -1,36 +1,39 @@
+import Button from 'components/Buttons/Button';
+import FixedButtons from 'components/Buttons/FixedButtons';
 import ContentItemControls from 'components/ContentItemControls/ContentItemControls';
-import DataLayout from 'components/DataLayout/DataLayout';
-import DataLayoutContentFrame from 'components/DataLayout/DataLayoutContentFrame';
-import DataLayoutTitle from 'components/DataLayout/DataLayoutTitle';
+import Inner from 'components/Inner/Inner';
 import { ConfirmModalInterface } from 'components/Modal/ConfirmModal/ConfirmModal';
 import { CreateRoleModalInterface } from 'components/Modal/CreateRoleModal/CreateRoleModal';
-import RequestError from 'components/RequestError/RequestError';
-import Spinner from 'components/Spinner/Spinner';
 import Table, { TableColumn } from 'components/Table/Table';
-import { ROUTE_CMS } from 'config/common';
+import Title from 'components/Title/Title';
+import { ROUTE_CMS, SORT_DESC } from 'config/common';
 import { CONFIRM_MODAL, CREATE_ROLE_MODAL } from 'config/modals';
-import {
-  CmsRoleFragment,
-  useCreateRoleMutation,
-  useDeleteRoleMutation,
-  useGetAllRolesQuery,
-} from 'generated/apolloComponents';
+import { COL_ROLES } from 'db/collectionNames';
+import { getDatabase } from 'db/mongodb';
+import { RoleInterface } from 'db/uiInterfaces';
+import { useCreateRoleMutation, useDeleteRoleMutation } from 'generated/apolloComponents';
 import { GET_ALL_ROLES_QUERY } from 'graphql/query/rolesQueries';
 import useMutationCallbacks from 'hooks/useMutationCallbacks';
+import AppContentWrapper from 'layout/AppLayout/AppContentWrapper';
+import { getFieldStringLocale } from 'lib/i18n';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { PagePropsInterface } from 'pages/_app';
 import * as React from 'react';
 import CmsLayout from 'layout/CmsLayout/CmsLayout';
-import { GetServerSidePropsContext, NextPage } from 'next';
-import { getAppInitialData } from 'lib/ssrUtils';
+import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next';
+import { castDbData, getAppInitialData } from 'lib/ssrUtils';
 
-const RolesContent: React.FC = () => {
+interface RolesConsumerInterface {
+  roles: RoleInterface[];
+}
+
+const pageTitle = 'Роли';
+
+const RolesConsumer: React.FC<RolesConsumerInterface> = ({ roles }) => {
   const router = useRouter();
   const { onCompleteCallback, onErrorCallback, showModal } = useMutationCallbacks({
     withModal: true,
-  });
-  const { data, loading, error } = useGetAllRolesQuery({
-    fetchPolicy: 'network-only',
   });
 
   const refetchQueries = [
@@ -53,17 +56,7 @@ const RolesContent: React.FC = () => {
     refetchQueries,
   });
 
-  if (loading) {
-    return <Spinner />;
-  }
-
-  if (error || !data) {
-    return <RequestError />;
-  }
-
-  const { getAllRoles } = data;
-
-  const columns: TableColumn<CmsRoleFragment>[] = [
+  const columns: TableColumn<RoleInterface>[] = [
     {
       headTitle: 'Название',
       accessor: 'name',
@@ -77,85 +70,127 @@ const RolesContent: React.FC = () => {
     {
       render: ({ dataItem }) => {
         return (
-          <ContentItemControls
-            justifyContent={'flex-end'}
-            testId={dataItem.name}
-            updateTitle={'Редактировать роль'}
-            updateHandler={() => {
-              router.push(`${ROUTE_CMS}/roles/${dataItem._id}`).catch((e) => console.log(e));
-            }}
-            deleteTitle={'Удалить роль'}
-            deleteHandler={() => {
-              showModal<ConfirmModalInterface>({
-                variant: CONFIRM_MODAL,
-                props: {
-                  testId: 'delete-role-modal',
-                  message:
-                    'Вы уверены, что хотите удалить роль? Всем пользователям с данной ролью будет назначена роль Гость.',
-                  confirm: () => {
-                    deleteRoleMutation({
-                      variables: { _id: dataItem._id },
-                    }).catch((e) => console.log(e));
+          <div className='flex justify-end'>
+            <ContentItemControls
+              testId={`${dataItem.name}`}
+              updateTitle={'Редактировать роль'}
+              updateHandler={() => {
+                router.push(`${ROUTE_CMS}/roles/${dataItem._id}`).catch((e) => console.log(e));
+              }}
+              deleteTitle={'Удалить роль'}
+              deleteHandler={() => {
+                showModal<ConfirmModalInterface>({
+                  variant: CONFIRM_MODAL,
+                  props: {
+                    testId: 'delete-role-modal',
+                    message:
+                      'Вы уверены, что хотите удалить роль? Всем пользователям с данной ролью будет назначена роль Гость.',
+                    confirm: () => {
+                      deleteRoleMutation({
+                        variables: { _id: dataItem._id },
+                      }).catch((e) => console.log(e));
+                    },
                   },
-                },
-              });
-            }}
-          />
+                });
+              }}
+            />
+          </div>
         );
       },
     },
   ];
 
   return (
-    <DataLayoutContentFrame>
-      <DataLayoutTitle
-        titleRight={
-          <ContentItemControls
-            testId={'roles'}
-            createTitle={'Добавить роль'}
-            createHandler={() => {
-              showModal<CreateRoleModalInterface>({
-                variant: CREATE_ROLE_MODAL,
-                props: {
-                  confirm: (values) => {
-                    createRoleMutation({
-                      variables: {
-                        input: values,
-                      },
-                    }).catch((e) => console.log(e));
+    <AppContentWrapper>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      <Inner>
+        <Title>{pageTitle}</Title>
+        <div className='relative'>
+          <div className='overflew-x-auto overflew-y-hidden'>
+            <Table<RoleInterface>
+              columns={columns}
+              data={roles}
+              testIdKey={'name'}
+              onRowDoubleClick={(dataItem) => {
+                router.push(`${ROUTE_CMS}/roles/${dataItem._id}`).catch((e) => console.log(e));
+              }}
+            />
+          </div>
+          <FixedButtons>
+            <Button
+              size={'small'}
+              onClick={() => {
+                showModal<CreateRoleModalInterface>({
+                  variant: CREATE_ROLE_MODAL,
+                  props: {
+                    confirm: (values) => {
+                      createRoleMutation({
+                        variables: {
+                          input: values,
+                        },
+                      }).catch((e) => console.log(e));
+                    },
                   },
-                },
-              });
-            }}
-          />
-        }
-      />
-      <Table<CmsRoleFragment> columns={columns} data={getAllRoles} testIdKey={'name'} />
-    </DataLayoutContentFrame>
+                });
+              }}
+              testId={'create-role'}
+            >
+              Добавить роль
+            </Button>
+          </FixedButtons>
+        </div>
+      </Inner>
+    </AppContentWrapper>
   );
 };
 
-const RolesRoute: React.FC = () => {
-  return (
-    <DataLayout
-      title={'Роли'}
-      filterResult={() => {
-        return <RolesContent />;
-      }}
-    />
-  );
-};
+interface RolesPageInterface extends PagePropsInterface, RolesConsumerInterface {}
 
-const Roles: NextPage<PagePropsInterface> = ({ pageUrls }) => {
+const RolesPage: NextPage<RolesPageInterface> = ({ pageUrls, roles }) => {
   return (
     <CmsLayout pageUrls={pageUrls}>
-      <RolesRoute />
+      <RolesConsumer roles={roles} />
     </CmsLayout>
   );
 };
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-  return getAppInitialData({ context, isCms: true });
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<RolesPageInterface>> => {
+  const { props } = await getAppInitialData({ context, isCms: true });
+  if (!props) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const db = await getDatabase();
+  const rolesCollection = db.collection<RoleInterface>(COL_ROLES);
+  const rolesAggregationResult = await rolesCollection
+    .aggregate([
+      {
+        $sort: {
+          _id: SORT_DESC,
+        },
+      },
+    ])
+    .toArray();
+
+  const roles = rolesAggregationResult.map((role) => {
+    return {
+      ...role,
+      name: getFieldStringLocale(role.nameI18n, props.sessionLocale),
+    };
+  });
+
+  return {
+    props: {
+      ...props,
+      roles: castDbData(roles),
+    },
+  };
 };
 
-export default Roles;
+export default RolesPage;

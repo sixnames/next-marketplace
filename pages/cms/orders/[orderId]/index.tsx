aa3072d6bd2b3769b607c6feb1b840c4ed09f654
sixnames <slava.kirshman@icloud.com>
2021-06-24 13:53:29 +0300

    cms order ssr query added

--- pages/cms/orders/[orderId]/index.tsx
+++ pages/cms/orders/[orderId]/index.tsx
@@ -1,28 +1,80 @@
 import Inner from 'components/Inner';
+import Title from 'components/Title';
+import { COL_ORDERS } from 'db/collectionNames';
+import { getDatabase } from 'db/mongodb';
+import { OrderInterface } from 'db/uiInterfaces';
+import AppContentWrapper from 'layout/AppLayout/AppContentWrapper';
 import CmsLayout from 'layout/CmsLayout/CmsLayout';
-import { useRouter } from 'next/router';
+import { ObjectId } from 'mongodb';
 import { PagePropsInterface } from 'pages/_app';
 import * as React from 'react';
-import { GetServerSidePropsContext, NextPage } from 'next';
-import { getAppInitialData } from 'lib/ssrUtils';
+import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next';
+import { castDbData, getAppInitialData } from 'lib/ssrUtils';
 
-const OrderPageConsumer: React.FC = () => {
-  const { query } = useRouter();
-  const { orderId } = query;
+interface OrderPageConsumerInterface {
+  order: OrderInterface;
+}
 
-  return <Inner testId={`order-details`}>Заказ №{orderId}</Inner>;
+const OrderPageConsumer: React.FC<OrderPageConsumerInterface> = ({ order }) => {
+  return (
+    <AppContentWrapper>
+      <Inner testId={`order-details`}>
+        <Title>Заказ №{order.itemId}</Title>
+      </Inner>
+    </AppContentWrapper>
+  );
 };
 
-const OrderPage: NextPage<PagePropsInterface> = ({ pageUrls }) => {
+interface OrderPageInterface extends OrderPageConsumerInterface, PagePropsInterface {}
+
+const OrderPage: NextPage<OrderPageInterface> = ({ pageUrls, order }) => {
   return (
-    <CmsLayout pageUrls={pageUrls}>
-      <OrderPageConsumer />
+    <CmsLayout pageUrls={pageUrls} title={`Заказ №${order.itemId}`}>
+      <OrderPageConsumer order={order} />
     </CmsLayout>
   );
 };
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-  return getAppInitialData({ context });
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<OrderPageInterface>> => {
+  const { query } = context;
+  const { props } = await getAppInitialData({ context });
+  if (!props || !query.orderId) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const { db } = await getDatabase();
+  const ordersCollection = db.collection<OrderInterface>(COL_ORDERS);
+  const orderAggregationResult = await ordersCollection
+    .aggregate([
+      {
+        $match: {
+          _id: new ObjectId(`${query.orderId}`),
+        },
+      },
+    ])
+    .toArray();
+  const initialOrder = orderAggregationResult[0];
+
+  if (!initialOrder) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const order: OrderInterface = {
+    ...initialOrder,
+  };
+
+  return {
+    props: {
+      ...props,
+      order: castDbData(order),
+    },
+  };
 };
 
 export default OrderPage;

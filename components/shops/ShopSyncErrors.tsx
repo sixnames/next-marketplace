8b6221c375c54ae0edd77ae6352f57801a8fd7bc
sixnames <slava.kirshman@icloud.com>
2021-06-24 09:34:16 +0300

    ShopSyncErrors table added

--- components/shops/ShopSyncErrors.tsx
+++ components/shops/ShopSyncErrors.tsx
@@ -1,4 +1,6 @@
+import Currency from 'components/Currency';
 import Inner from 'components/Inner';
+import Table, { TableColumn } from 'components/Table';
 import { NotSyncedProductInterface } from 'db/uiInterfaces';
 import AppShopLayout, { AppShopLayoutInterface } from 'layout/AppLayout/AppShopLayout';
 import * as React from 'react';
@@ -12,13 +14,39 @@ const ShopSyncErrors: React.FC<ShopSyncErrorsInterface> = ({
   notSyncedProducts,
   basePath,
 }) => {
-  console.log(notSyncedProducts);
+  const columns: TableColumn<NotSyncedProductInterface>[] = [
+    {
+      accessor: 'barcode',
+      headTitle: 'Штрих-код',
+      render: ({ cellData }) => cellData,
+    },
+    {
+      accessor: 'name',
+      headTitle: 'Название',
+      render: ({ cellData }) => cellData,
+    },
+    {
+      accessor: 'available',
+      headTitle: 'В наличии',
+      render: ({ cellData }) => cellData,
+    },
+    {
+      accessor: 'price',
+      headTitle: 'Цена',
+      render: ({ cellData }) => <Currency value={cellData} />,
+    },
+  ];
+
   return (
     <AppShopLayout shop={shop} basePath={basePath}>
       <Inner testId={'shop-sync-errors-page'}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum, eaque fuga hic illo
-        quod sapiente velit veritatis! Ab adipisci aspernatur deserunt dignissimos dolore earum hic
-        incidunt provident quasi quos?
+        <div className='overflow-x-auto overflow-y-hidden'>
+          <Table<NotSyncedProductInterface>
+            testIdKey={'name'}
+            columns={columns}
+            data={notSyncedProducts}
+          />
+        </div>
       </Inner>
     </AppShopLayout>
   );

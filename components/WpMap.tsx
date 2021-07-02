5b217ceb2d00b3df9dbbb6867ce72edffbcc726a
sixnames <slava.kirshman@icloud.com>
2021-07-02 20:27:16 +0300

    ShopsMap refactored for WpMap component

--- components/WpMap.tsx
+++ components/WpMap.tsx
@@ -30,7 +30,7 @@ interface WpMapCenterInterface {
   lng: number;
 }
 
-interface WpMapInterface {
+export interface WpMapInterface {
   markers?: WpMapMarkerInterface[];
   testId?: string;
   center?: WpMapCenterInterface;
@@ -103,7 +103,7 @@ const WpMap: React.FC<WpMapInterface> = ({
               key={`${_id}`}
               position={address.formattedCoordinates}
               icon={{
-                url: icon,
+                url: icon || '/marker.svg',
                 scaledSize: new window.google.maps.Size(40, 40),
               }}
               onClick={() => {
@@ -115,9 +115,12 @@ const WpMap: React.FC<WpMapInterface> = ({
             />
           );
         })}
-        {selected ? (
-          <InfoWindow position={selected.address} onCloseClick={() => setSelected(null)}>
-            <div>
+        {selected && selected.address && selected.address.formattedCoordinates ? (
+          <InfoWindow
+            position={selected.address.formattedCoordinates}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div className='pb-2'>
               <div className='mb-3 font-bold text-xl text-black'>{selected.name}</div>
               <div className='text-[1rem] text-black'>{selected.address?.formattedAddress}</div>
             </div>

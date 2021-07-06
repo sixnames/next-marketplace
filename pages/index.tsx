19d30695061f57aa65ada9cfdd6151e5ca37790e
sixnames <slava.kirshman@icloud.com>
2021-07-06 17:56:51 +0300

    secondary banners ui added

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,14 @@ import {
   CATALOGUE_OPTION_SEPARATOR,
   ROUTE_CATALOGUE,
   CATALOGUE_TOP_SHOPS_LIMIT,
+  ROUTE_DOCS_PAGES,
 } from 'config/common';
 import { useConfigContext } from 'context/configContext';
 import { COL_SHOP_PRODUCTS, COL_SHOPS } from 'db/collectionNames';
 import { getDatabase } from 'db/mongodb';
 import {
+  PageInterface,
+  PagesGroupInterface,
   ProductInterface,
   ShopInterface,
   ShopProductInterface,
@@ -39,24 +42,22 @@ interface HomeRoutInterface {
   topProducts: ProductInterface[];
   topShops: ShopInterface[];
   topFilters: TopFilterInterface[];
+  sliderPages: PageInterface[];
+  bannerPages: PageInterface[];
 }
 
-const bannersConfig = [
-  {
-    src: `https://${process.env.OBJECT_STORAGE_DOMAIN}/banners/banner-1.jpg`,
-  },
-  {
-    src: `https://${process.env.OBJECT_STORAGE_DOMAIN}/banners/banner-2.jpg`,
-  },
-  {
-    src: `https://${process.env.OBJECT_STORAGE_DOMAIN}/banners/banner-3.jpg`,
-  },
-];
-
-const HomeRoute: React.FC<HomeRoutInterface> = ({ topProducts, topShops, topFilters }) => {
+const HomeRoute: React.FC<HomeRoutInterface> = ({
+  topProducts,
+  topShops,
+  sliderPages,
+  bannerPages,
+  topFilters,
+}) => {
   const { getSiteConfigSingleValue } = useConfigContext();
   const configTitle = getSiteConfigSingleValue('pageDefaultTitle');
   const sectionClassName = `mb-14 sm:mb-28`;
+
+  console.log({ sliderPages });
 
   return (
     <React.Fragment>
@@ -97,25 +98,45 @@ const HomeRoute: React.FC<HomeRoutInterface> = ({ topProducts, topShops, topFilt
           </section>
         ) : null}
 
-        <section className={sectionClassName}>
-          <div className='text-2xl mb-4 font-medium'>
-            <h2>Акции</h2>
-          </div>
-          <HorizontalScroll>
-            {bannersConfig.map(({ src }) => {
-              return (
-                <div
-                  className='flex min-w-[80vw] sm:min-w-[30rem] overflow-hidden rounded-lg'
-                  key={`${src}`}
-                >
-                  <Link className='block' href={ROUTE_CATALOGUE_DEFAULT_RUBRIC}>
-                    <img src={src} width='526' height='360' alt={src} title={src} />
-                  </Link>
-                </div>
-              );
-            })}
-          </HorizontalScroll>
-        </section>
+        {bannerPages.length > 0 ? (
+          <section className={sectionClassName}>
+            <div className='text-2xl mb-4 font-medium'>
+              <h2>Акции</h2>
+            </div>
+            <HorizontalScroll>
+              {bannerPages.map(({ secondaryBanner, name, slug }) => {
+                if (!secondaryBanner) {
+                  return null;
+                }
+
+                return (
+                  <div
+                    className='relative flex min-w-[80vw] sm:min-w-[30rem] overflow-hidden rounded-lg'
+                    key={`${secondaryBanner.url}`}
+                  >
+                    <Link
+                      className='block z-10'
+                      target={'_blank'}
+                      href={`${ROUTE_DOCS_PAGES}/${slug}`}
+                    >
+                      <img
+                        className='block'
+                        src={`${secondaryBanner.url}`}
+                        width='526'
+                        height='360'
+                        alt={`${name}`}
+                        title={`${name}`}
+                      />
+                      <span className='absolute block inset-0 p-8 text-white'>
+                        <span className='block font-medium text-3xl max-h-[160px]'>{name}</span>
+                      </span>
+                    </Link>
+                  </div>
+                );
+              })}
+            </HorizontalScroll>
+          </section>
+        ) : null}
 
         {topFilters.length > 0 ? (
           <section className={sectionClassName}>
@@ -149,10 +170,23 @@ const HomeRoute: React.FC<HomeRoutInterface> = ({ topProducts, topShops, topFilt
 
 interface HomeInterface extends SiteLayoutProviderInterface, HomeRoutInterface {}
 
-const Home: NextPage<HomeInterface> = ({ topProducts, topShops, topFilters, ...props }) => {
+const Home: NextPage<HomeInterface> = ({
+  topProducts,
+  topShops,
+  topFilters,
+  bannerPages,
+  sliderPages,
+  ...props
+}) => {
   return (
     <SiteLayoutProvider {...props}>
-      <HomeRoute topProducts={topProducts} topFilters={topFilters} topShops={topShops} />
+      <HomeRoute
+        topProducts={topProducts}
+        topFilters={topFilters}
+        topShops={topShops}
+        sliderPages={sliderPages}
+        bannerPages={bannerPages}
+      />
     </SiteLayoutProvider>
   );
 };
@@ -173,7 +207,8 @@ export async function getServerSideProps(
     };
   }
 
-  const { companySlug, sessionCity, sessionLocale, company } = props;
+  const { companySlug, sessionCity, sessionLocale, company, footerPageGroups, headerPageGroups } =
+    props;
 
   const companyRubricsMatch = company ? { companyId: new ObjectId(company._id) } : {};
   const shopProductsAggregation = await shopProductsCollection
@@ -347,12 +382,44 @@ export async function getServerSideProps(
     });
   });
 
+  const allPageGroups: PagesGroupInterface[] = castDbData([
+    ...headerPageGroups,
+    ...footerPageGroups,
+  ]);
+  const sliderPages: PageInterface[] = [];
+  const bannerPages: PageInterface[] = [];
+
+  allPageGroups.forEach((pagesGroup) => {
+    const { pages } = pagesGroup;
+    (pages || []).forEach((page) => {
+      // main slide
+      const showAsMainBanner = page.showAsMainBanner && page.mainBanner?.url;
+      const existInSliderPages = sliderPages.find(({ _id }) => {
+        return _id === page._id;
+      });
+      if (!existInSliderPages && showAsMainBanner) {
+        sliderPages.push(page);
+      }
+
+      // banners
+      const showAsSecondaryBanner = page.showAsSecondaryBanner && page.secondaryBanner?.url;
+      const existInBannerPages = bannerPages.find(({ _id }) => {
+        return _id === page._id;
+      });
+      if (!existInBannerPages && showAsSecondaryBanner) {
+        bannerPages.push(page);
+      }
+    });
+  });
+
   return {
     props: {
       ...props,
       topFilters: castDbData(topFilters),
       topProducts: castDbData(products),
       topShops: castDbData(topShops),
+      sliderPages,
+      bannerPages,
     },
   };
 }

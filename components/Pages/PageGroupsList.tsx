acb4cf7543a3831b218a49984d7da3b47c873cfa
sixnames <slava.kirshman@icloud.com>
2021-07-13 11:32:02 +0300

    page template mutations fixed

--- components/Pages/PageGroupsList.tsx
+++ components/Pages/PageGroupsList.tsx
@@ -140,6 +140,7 @@ const PageGroupsList: React.FC<PageGroupsListInterface> = ({
               props: {
                 companySlug,
                 validationSchema: createPagesGroupValidationSchema,
+                isTemplate,
               },
             });
           }}

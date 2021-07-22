28ece51f402797597cd9c97fb7232985fb9eb2a6
sixnames <slava.kirshman@icloud.com>
2021-07-22 09:13:02 +0300

    seedNewDb util added

--- cypress/fixtures/utils/seedNewDb.ts
+++ cypress/fixtures/utils/seedNewDb.ts
@@ -1,4 +1,30 @@
+import { Seeder } from 'mongo-seeding';
+import { uploadTestAssets } from '../../../tests/seedStageDb';
 import { getProdDb, GetProdDd, updateIndexes } from './getProdDb';
+const path = require('path');
+
+const bucketName = `${process.env.STAGE_OBJECT_STORAGE_BUCKET_NAME}`;
+const tlsCAFile = path.join(process.cwd(), 'db', 'root.crt');
+
+const config = {
+  database: {
+    protocol: 'mongodb',
+    host: `${process.env.STAGE_MONGO_TEST_HOST}`,
+    port: +`${process.env.STAGE_MONGO_TEST_PORT}`,
+    username: `${process.env.STAGE_MONGO_TEST_USER_NAME}`,
+    password: `${process.env.STAGE_MONGO_TEST_USER_PWD}`,
+    name: `${process.env.STAGE_MONGO_DB_NAME}`,
+    options: {
+      tls: 'true',
+      tlsCAFile,
+      replicaSet: `${process.env.MONGO_DB_RS}`,
+      authSource: `${process.env.STAGE_MONGO_DB_NAME}`,
+    },
+  },
+  dropDatabase: false,
+  dropCollections: true,
+  databaseReconnectTimeout: 10000,
+};
 
 const dbConfig: GetProdDd = {
   uri: '',
@@ -6,15 +32,32 @@ const dbConfig: GetProdDd = {
 };
 
 async function seedNewDb() {
-  const { db, client } = await getProdDb(dbConfig);
+  // seed
+  const seeder = new Seeder(config);
+  const collections = seeder.readCollectionsFromPath(
+    path.resolve(process.cwd(), 'cypress/fixtures/initialData/collections'),
+    {
+      extensions: ['json', 'ts'],
+    },
+  );
 
-  console.log(db);
+  try {
+    await seeder.import(collections);
+    console.log('Initial data seeded');
+
+    // upload assets
+    await uploadTestAssets(`./cypress/fixtures/initialData/assets/`, bucketName);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // create indexes
+  const { db, client } = await getProdDb(dbConfig);
+  console.log('Creating indexes');
+  await updateIndexes(db);
 
   // disconnect form db
   await client.close();
-
-  // create indexes
-  await updateIndexes(db);
 }
 
 (() => {

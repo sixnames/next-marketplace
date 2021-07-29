e8f0a3e30063fa6a41cc0a4f72a8d0b651a8b34f
sixnames <slava.kirshman@icloud.com>
2021-07-29 15:46:46 +0300

    CardDefaultLayout component added

---
+++ config/constantSelects.ts
@@ -0,0 +1,18 @@
+const LAYOUT_DEFAULT = 'default';
+
+export interface CardLayoutOptionInterface {
+  _id: string;
+  asset: string;
+}
+
+export const CARD_LAYOUT_HALF_COLUMNS = 'half-columns';
+export const CARD_LAYOUT_OPTIONS: CardLayoutOptionInterface[] = [
+  {
+    _id: LAYOUT_DEFAULT,
+    asset: '/layout/card/default.jpg',
+  },
+  {
+    _id: CARD_LAYOUT_HALF_COLUMNS,
+    asset: '/layout/card/half-columns.jpg',
+  },
+];

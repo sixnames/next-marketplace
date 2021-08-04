a0537d8e4845f9f3453bcf778861d011585b070c
sixnames <slava.kirshman@icloud.com>
2021-08-04 16:58:48 +0300

    FormikImageUpload updated with WpImageUpload component

--- components/FormElements/Upload/FormikImageUpload.tsx
+++ components/FormElements/Upload/FormikImageUpload.tsx
@@ -1,12 +1,7 @@
+import WpImageUpload from 'components/FormElements/Upload/WpImageUpload';
 import * as React from 'react';
-import { Field, FieldProps, useFormikContext } from 'formik';
-import Icon from 'components/Icon';
-import InputLine from '../Input/InputLine';
-import classes from './FormikImageUpload.module.css';
+import { Field, FieldProps } from 'formik';
 import { FormikInputPropsInterface } from '../Input/FormikInput';
-import ButtonCross from 'components/ButtonCross';
-import { useDropzone } from 'react-dropzone';
-import Tooltip from 'components/Tooltip';
 import { get } from 'lodash';
 
 interface FormikImageUploadInterface extends FormikInputPropsInterface {
@@ -19,45 +14,10 @@ interface FormikImageUploadInterface extends FormikInputPropsInterface {
 
 const FormikImageUpload: React.FC<FormikImageUploadInterface> = ({
   name,
-  label,
-  isRequired,
-  lineClass,
-  labelPostfix,
-  labelLink,
-  low,
-  tooltip,
-  testId,
-  isHorizontal,
-  description,
-  width = '6rem',
-  height = '6rem',
-  children,
-  lineContentClass,
   setImageHandler,
-  showInlineError,
-  format,
+  ...props
 }) => {
   const [files, setFiles] = React.useState<any[]>([]);
-  const { setFieldValue } = useFormikContext<any>();
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop: (acceptedFiles) => {
-      setFieldValue(name, acceptedFiles);
-
-      if (setImageHandler) {
-        setImageHandler(acceptedFiles);
-      }
-
-      // Set preview files
-      setFiles(
-        acceptedFiles.map((file: any) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          }),
-        ),
-      );
-    },
-    accept: format,
-  });
 
   // Make sure to revoke the data uris to avoid memory leaks
   React.useEffect(
@@ -67,64 +27,40 @@ const FormikImageUpload: React.FC<FormikImageUploadInterface> = ({
     [files],
   );
 
-  function removeImageHandler() {
-    setFieldValue(name, []);
-    setFiles([]);
-  }
-
   return (
     <Field name={name}>
-      {({ field, form: { errors } }: FieldProps) => {
+      {({ field, form: { errors, setFieldValue } }: FieldProps) => {
         const currentValue = field.value && field.value.length > 0 ? field.value[0] : null;
         const imageSrc =
           typeof currentValue === 'string' ? currentValue : files[0]?.preview || null;
         const error = get(errors, name);
 
         return (
-          <InputLine
-            isRequired={isRequired}
-            name={name}
-            lineClass={lineClass}
-            label={label}
-            labelPostfix={labelPostfix}
-            labelLink={labelLink}
-            low={low}
-            isHorizontal={isHorizontal}
-            description={description}
-            lineContentClass={lineContentClass}
-            showInlineError={showInlineError}
+          <WpImageUpload
             error={error}
-          >
-            <Tooltip title={tooltip}>
-              <div className={classes.frame} style={{ width, height }}>
-                <span className={classes.inputFrame} {...getRootProps()} data-cy={testId}>
-                  <input {...getInputProps()} className={classes.input} name={name} id={name} />
-                </span>
+            previewUrl={imageSrc}
+            removeImageHandler={() => {
+              setFieldValue(name, []);
+              setFiles([]);
+            }}
+            uploadImageHandler={(acceptedFiles) => {
+              setFieldValue(name, acceptedFiles);
 
-                {imageSrc ? (
-                  <div className={classes.preview}>
-                    <img
-                      src={imageSrc}
-                      width='100'
-                      height='100'
-                      alt={''}
-                      data-cy={`${testId}-image`}
-                    />
-                    <ButtonCross
-                      size={'small'}
-                      iconSize={'smaller'}
-                      onClick={removeImageHandler}
-                      testId={`${testId}-remove`}
-                      className={classes.remove}
-                    />
-                  </div>
-                ) : (
-                  <Icon name={'image'} className={classes.noImage} />
-                )}
-              </div>
-            </Tooltip>
-            {children}
-          </InputLine>
+              if (setImageHandler) {
+                setImageHandler(acceptedFiles);
+              }
+
+              // Set preview files
+              setFiles(
+                acceptedFiles.map((file: any) =>
+                  Object.assign(file, {
+                    preview: URL.createObjectURL(file),
+                  }),
+                ),
+              );
+            }}
+            {...props}
+          />
         );
       }}
     </Field>

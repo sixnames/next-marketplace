536711a66619356d7ef670f9d52f3dd958578623
sixnames <slava.kirshman@icloud.com>
2021-08-24 19:50:51 +0300

    updateProductCategory mutation transaction added

--- schema/ProductMutations.ts
+++ schema/ProductMutations.ts
@@ -1528,130 +1528,154 @@ export const ProductMutations = extendType({
         ),
       },
       resolve: async (_root, args, context): Promise<ProductPayloadModel> => {
+        const { db, client } = await getDatabase();
+        const { getApiMessage } = await getRequestParams(context);
+        const productsCollection = db.collection<ProductModel>(COL_PRODUCTS);
+        const shopProductsCollection = db.collection<ShopProductModel>(COL_SHOP_PRODUCTS);
+        const categoriesCollection = db.collection<CategoryModel>(COL_CATEGORIES);
+
+        const session = client.startSession();
+
+        let mutationPayload: ProductPayloadModel = {
+          success: false,
+          message: await getApiMessage(`products.update.error`),
+        };
+
         try {
-          const { db } = await getDatabase();
-          const { getApiMessage } = await getRequestParams(context);
-          const productsCollection = db.collection<ProductModel>(COL_PRODUCTS);
-          const shopProductsCollection = db.collection<ShopProductModel>(COL_SHOP_PRODUCTS);
-          const categoriesCollection = db.collection<CategoryModel>(COL_CATEGORIES);
-          const { input } = args;
-          const { productId, categoryId } = input;
-
-          // Permission
-          const { allow, message } = await getOperationPermission({
-            context,
-            slug: 'updateProduct',
-          });
-          if (!allow) {
-            return {
-              success: true,
-              message,
-            };
-          }
-
-          // Check product availability
-          const product = await productsCollection.findOne({ _id: productId });
-          if (!product) {
-            return {
-              success: true,
-              message: await getApiMessage(`products.update.notFound`),
-            };
-          }
-
-          // Check category availability
-          const category = await categoriesCollection.findOne({ _id: categoryId });
-          if (!category) {
-            return {
-              success: true,
-              message: await getApiMessage(`products.update.error`),
-            };
-          }
-
-          // Get category siblings
-          let countSelectedSiblings = 0;
-          if (category.parentId) {
-            countSelectedSiblings = await categoriesCollection.countDocuments({
-              _id: {
-                $ne: categoryId,
-              },
-              parentId: category.parentId,
-              slug: {
-                $in: product.selectedOptionsSlugs,
-              },
+          await session.withTransaction(async () => {
+            // Permission
+            const { allow, message } = await getOperationPermission({
+              context,
+              slug: 'updateProduct',
             });
-          }
+            if (!allow) {
+              mutationPayload = {
+                success: false,
+                message,
+              };
+              await session.abortTransaction();
+              return;
+            }
 
-          // Toggle category in product
-          const selected = product.selectedOptionsSlugs.some((slug) => slug === category.slug);
-          const categoryParentTreeSlugs = await getParentTreeSlugs({
-            _id: category._id,
-            collectionName: COL_CATEGORIES,
-            acc: [],
+            const { input } = args;
+            const { productId, categoryId } = input;
+
+            // Check product availability
+            const product = await productsCollection.findOne({ _id: productId });
+            if (!product) {
+              mutationPayload = {
+                success: false,
+                message: await getApiMessage(`products.update.notFound`),
+              };
+              await session.abortTransaction();
+              return;
+            }
+
+            // Check category availability
+            const category = await categoriesCollection.findOne({ _id: categoryId });
+            if (!category) {
+              mutationPayload = {
+                success: false,
+                message: await getApiMessage(`products.update.error`),
+              };
+              await session.abortTransaction();
+              return;
+            }
+
+            // Get category siblings
+            let countSelectedSiblings = 0;
+            if (category.parentId) {
+              countSelectedSiblings = await categoriesCollection.countDocuments({
+                _id: {
+                  $ne: categoryId,
+                },
+                parentId: category.parentId,
+                slug: {
+                  $in: product.selectedOptionsSlugs,
+                },
+              });
+            }
+
+            // Toggle category in product
+            const selected = product.selectedOptionsSlugs.some((slug) => slug === category.slug);
+            const categoryParentTreeSlugs = await getParentTreeSlugs({
+              _id: category._id,
+              collectionName: COL_CATEGORIES,
+              acc: [],
+            });
+
+            let updater: Record<string, any> = {
+              $addToSet: {
+                selectedOptionsSlugs: {
+                  $each: categoryParentTreeSlugs,
+                },
+              },
+            };
+            if (selected) {
+              if (countSelectedSiblings > 0) {
+                updater = {
+                  $pull: {
+                    selectedOptionsSlugs: category.slug,
+                  },
+                };
+              } else {
+                updater = {
+                  $pullAll: {
+                    selectedOptionsSlugs: categoryParentTreeSlugs,
+                  },
+                };
+              }
+            }
+
+            // update product
+            const updatedProductResult = await productsCollection.findOneAndUpdate(
+              {
+                _id: productId,
+              },
+              updater,
+            );
+            const updatedProduct = updatedProductResult.value;
+            if (!updatedProductResult.ok || !updatedProduct) {
+              mutationPayload = {
+                success: false,
+                message: await getApiMessage(`products.update.error`),
+              };
+              await session.abortTransaction();
+              return;
+            }
+
+            // update shop products
+            const updatedShopProductsResult = await shopProductsCollection.updateMany(
+              {
+                productId,
+              },
+              updater,
+            );
+            if (!updatedShopProductsResult.result.ok) {
+              mutationPayload = {
+                success: false,
+                message: await getApiMessage(`products.update.error`),
+              };
+              await session.abortTransaction();
+              return;
+            }
+
+            mutationPayload = {
+              success: true,
+              message: await getApiMessage('shopProducts.update.success'),
+              payload: updatedProduct,
+            };
           });
 
-          let updater: Record<string, any> = {
-            $addToSet: {
-              selectedOptionsSlugs: {
-                $each: categoryParentTreeSlugs,
-              },
-            },
-          };
-          if (selected) {
-            if (countSelectedSiblings > 0) {
-              updater = {
-                $pull: {
-                  selectedOptionsSlugs: category.slug,
-                },
-              };
-            } else {
-              updater = {
-                $pullAll: {
-                  selectedOptionsSlugs: categoryParentTreeSlugs,
-                },
-              };
-            }
-          }
-
-          // update product
-          const updatedProductResult = await productsCollection.findOneAndUpdate(
-            {
-              _id: productId,
-            },
-            updater,
-          );
-          const updatedProduct = updatedProductResult.value;
-          if (!updatedProductResult.ok || !updatedProduct) {
-            return {
-              success: true,
-              message: await getApiMessage(`products.update.error`),
-            };
-          }
-
-          // update shop products
-          const updatedShopProductsResult = await shopProductsCollection.updateMany(
-            {
-              productId,
-            },
-            updater,
-          );
-          if (!updatedShopProductsResult.result.ok) {
-            return {
-              success: true,
-              message: await getApiMessage(`products.update.error`),
-            };
-          }
-
-          return {
-            success: true,
-            message: await getApiMessage(`products.update.success`),
-            payload: updatedProduct,
-          };
+          return mutationPayload;
         } catch (e) {
           console.log(e);
           return {
             success: false,
             message: getResolverErrorMessage(e),
           };
+        } finally {
+          await session.endSession();
         }
       },
     });

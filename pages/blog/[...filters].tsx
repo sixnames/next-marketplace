00517be37611e39960801736ebc609320a83fb9a
sixnames <slava.kirshman@icloud.com>
2021-08-31 11:37:27 +0300

    blog route updated for redirect

--- pages/blog/[...filters].tsx
+++ pages/blog/[...filters].tsx
@@ -1,14 +1,43 @@
+import { BlogPostInterface } from 'db/uiInterfaces';
+import SiteLayout from 'layout/SiteLayout/SiteLayout';
+import { SiteLayoutProviderInterface } from 'layout/SiteLayoutProvider';
+import { getSiteInitialData } from 'lib/ssrUtils';
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import * as React from 'react';
 
-const BlogListPage: React.FC = () => {
-  return <div />;
+interface BlogListPageConsumerInterface {
+  posts: BlogPostInterface[];
+}
+
+interface BlogListPageInterface
+  extends SiteLayoutProviderInterface,
+    BlogListPageConsumerInterface {}
+
+const BlogListPage: React.FC<BlogListPageInterface> = ({ ...props }) => {
+  return (
+    <SiteLayout {...props}>
+      <div />
+    </SiteLayout>
+  );
 };
 
-export const getServerSideProps = () => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<BlogListPageInterface>> => {
+  const { props } = await getSiteInitialData({
+    context,
+  });
+
+  if (!props || !props.initialData.configs.showBlog) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
-    redirect: {
-      permanent: true,
-      destination: `/`,
+    props: {
+      ...props,
+      posts: [],
     },
   };
 };

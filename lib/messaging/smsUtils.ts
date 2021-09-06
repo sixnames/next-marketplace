ac0dac2326d47b4478b8719bf1839a0dcf201d27
sixnames <slava.kirshman@icloud.com>
2021-09-06 15:15:59 +0300

    smsSender util added

--- lib/messaging/smsUtils.ts
+++ lib/messaging/smsUtils.ts
@@ -1,21 +1,43 @@
 import fetch from 'node-fetch';
 import qs from 'qs';
 
-export async function smsSender() {
-  const apiKey = process.env.SMS_API_KEY;
-  if (!apiKey) {
-    return;
+interface SmsSenderInterface {
+  text: string;
+  sign: string;
+  numbers: string[];
+}
+
+export async function smsSender({ text, sign, numbers }: SmsSenderInterface) {
+  try {
+    const apiKey = process.env.SMS_API_KEY;
+    const apiEmail = process.env.SMS_API_EMAIL;
+    if (!apiKey || !apiEmail) {
+      return;
+    }
+
+    // params
+    const params = {
+      text,
+      sign,
+      shortLink: 1,
+    };
+    const paramsAsString = qs.stringify(params);
+
+    // numbers
+    const numbersAsString = numbers
+      .reduce((acc: string[], number) => {
+        return [...acc, `numbers[]=${number}`];
+      }, [])
+      .join('&');
+
+    // final url params
+    const finalParamsArray = [paramsAsString, numbersAsString];
+    const finalParamsAsString = finalParamsArray.join('&');
+
+    const url = `https://${apiEmail}:${apiKey}@gate.smsaero.ru/v2/sms/send?${finalParamsAsString}`;
+    await fetch(url);
+  } catch (e) {
+    console.log(`smsSender Error`);
+    console.log(e);
   }
-  const params = {
-    text: `Тестовое сообщение
-    с переносом на новую строку.
-    И ещё строка`,
-    sign: `SMS Aero`,
-  };
-  const paramsAsString = qs.stringify(params);
-  const url = `https://info@winepoint.ru:${apiKey}@gate.smsaero.ru/v2/sms/send?numbers[]=79262742057&${paramsAsString}`;
-  console.log(url);
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
 }

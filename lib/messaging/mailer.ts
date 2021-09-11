8b4d6b779cc2983cc22f18eee4318fec487d84d4
sixnames <slava.kirshman@icloud.com>
2021-09-11 18:55:29 +0300

    mailer util subject fixed

--- lib/messaging/mailer.ts
+++ lib/messaging/mailer.ts
@@ -25,6 +25,7 @@ export const sendEmail = async ({
   locale,
 }: SendEmailInterface) => {
   try {
+    // get email api configs for current company
     const { db } = await getDatabase();
     const configsCollection = db.collection<ConfigModel>(COL_CONFIGS);
     const emailApiConfigs = await configsCollection
@@ -56,11 +57,8 @@ export const sendEmail = async ({
       return;
     }
 
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    // const testAccount = await nodemailer.createTestAccount();
-
-    const config = {
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
       host: emailApiHost,
       port: 465,
       secure: true,
@@ -68,14 +66,11 @@ export const sendEmail = async ({
         user: emailApiLogin,
         pass: emailApiPassword,
       },
-    };
-
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport(config);
+    });
 
     // send mail with defined transport object
     await transporter.sendMail({
-      from: '"Hello" <order@winepoint.ru>', // sender address '"Fred Foo 👻" <foo@example.com>'
+      from: `<${emailApiLogin}>`, // sender address '"Fred Foo 👻" <foo@example.com>'
       to, // list of receivers
       subject, // Subject line
       text, // plain text body
@@ -84,13 +79,6 @@ export const sendEmail = async ({
         content,
       }),
     });
-
-    // console.log(`Message sent: ${info.messageId}`);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (e) {
     if (process.env.NODE_ENV !== 'test') {
       console.error('Email message not sent\n', e);

0f00e7799d657573414b1a3e5ec3eb2dd949cf0b
sixnames <slava.kirshman@icloud.com>
2021-09-21 13:16:33 +0300

    cms filter selected attribute ui added

--- components/AppContentFilter.tsx
+++ components/AppContentFilter.tsx
@@ -1,10 +1,14 @@
 import FilterCheckbox from 'components/FilterCheckbox';
 import Icon from 'components/Icon';
+import FilterLink from 'components/Link/FilterLink';
 import Link from 'components/Link/Link';
 import { CATALOGUE_PRICE_KEY } from 'config/common';
 import { useConfigContext } from 'context/configContext';
 import { useLocaleContext } from 'context/localeContext';
-import { CatalogueFilterAttributeInterface } from 'db/uiInterfaces';
+import {
+  CatalogueFilterAttributeInterface,
+  CatalogueFilterAttributeOptionInterface,
+} from 'db/uiInterfaces';
 import * as React from 'react';
 
 interface AppContentFilterAttributeInterface {
@@ -65,6 +69,31 @@ const AppContentFilterAttribute: React.FC<AppContentFilterAttributeInterface> = 
   );
 };
 
+function getSelectedOptions(
+  option: CatalogueFilterAttributeOptionInterface,
+  acc: CatalogueFilterAttributeOptionInterface[],
+): CatalogueFilterAttributeOptionInterface[] {
+  const newAcc = [...acc];
+  if (option.isSelected) {
+    newAcc.push(option);
+  }
+  if (!option.options || option.options.length < 1) {
+    return newAcc;
+  }
+
+  const nestedOptions = option.options.reduce(
+    (nestedAcc: CatalogueFilterAttributeOptionInterface[], nestedOption) => {
+      const nestedOptions = getSelectedOptions(nestedOption, nestedAcc);
+      return [...nestedAcc, ...nestedOptions];
+    },
+    [],
+  );
+  nestedOptions.forEach((option) => {
+    newAcc.push(option);
+  });
+  return newAcc;
+}
+
 interface AppContentFilterInterface {
   attributes: CatalogueFilterAttributeInterface[];
   selectedAttributes: CatalogueFilterAttributeInterface[];
@@ -77,12 +106,60 @@ const AppContentFilter: React.FC<AppContentFilterInterface> = ({
   selectedAttributes,
   className,
 }) => {
-  console.log(selectedAttributes);
+  const { currency } = useLocaleContext();
+
   return (
-    <div className={className ? className : ''}>
-      {attributes.map((attribute) => {
-        return <AppContentFilterAttribute attribute={attribute} key={`${attribute._id}`} />;
-      })}
+    <div>
+      {selectedAttributes.length > 0 ? (
+        <div className='mb-8'>
+          <div className='mb-3 font-medium text-lg text-secondary-text'>Выбранные фильтры</div>
+
+          {selectedAttributes.map((attribute, attributeIndex) => {
+            const { name, clearSlug, options, isSelected, metric, slug } = attribute;
+            const isPrice = slug === CATALOGUE_PRICE_KEY;
+            const postfix = isPrice ? ` ${currency}` : metric ? ` ${metric}` : null;
+            const selectedOptions = options.reduce(
+              (acc: CatalogueFilterAttributeOptionInterface[], option) => {
+                return [...acc, ...getSelectedOptions(option, [])];
+              },
+              [],
+            );
+
+            return (
+              <div className='mb-4' key={slug}>
+                <div className={`flex mb-2 items-baseline`}>
+                  <span className={`font-medium text-lg`}>{name}</span>
+                  {isSelected ? (
+                    <Link href={clearSlug} className={`ml-4`}>
+                      Очистить
+                    </Link>
+                  ) : null}
+                </div>
+
+                <div className='flex flex-wrap gap-2'>
+                  {selectedOptions.map((option, optionIndex) => {
+                    const testId = `catalogue-option-${attributeIndex}-${optionIndex}`;
+                    return (
+                      <FilterLink
+                        withCross
+                        option={option}
+                        key={option.slug}
+                        testId={testId}
+                        postfix={postfix}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+      <div className={className ? className : ''}>
+        {attributes.map((attribute) => {
+          return <AppContentFilterAttribute attribute={attribute} key={`${attribute._id}`} />;
+        })}
+      </div>
     </div>
   );
 };

3a36e198fa2b71ad028739ff3f99df428eaa23b3
sixnames <slava.kirshman@icloud.com>
2021-09-28 13:32:26 +0300

    logs added fixed 7

--- pages/api/product/uniqueness/[productId].ts
+++ pages/api/product/uniqueness/[productId].ts
@@ -1,20 +1,29 @@
 import { COL_PRODUCTS } from 'db/collectionNames';
 import { ProductModel } from 'db/dbModels';
 import { getDatabase } from 'db/mongodb';
+import { parseRestApiFormData } from 'lib/restApi';
 import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { db } = await getDatabase();
     const productsCollection = db.collection<ProductModel>(COL_PRODUCTS);
+    const formData = await parseRestApiFormData(req);
 
-    console.log('');
-    console.log('');
+    console.log(' ');
+    console.log('req');
+    console.log(req);
+    console.log(' ');
     console.log('req body >>>>>>>>>');
-    console.log('');
-    console.log(req.body);
-    console.log('');
+    console.log(formData);
+    console.log(' ');
 
     const productId = new ObjectId(`${req.query.productId}`);
     const product = await productsCollection.findOne({

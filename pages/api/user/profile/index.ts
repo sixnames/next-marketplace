b8a37d263f29a8650a3136580828c3b43f077cfa
sixnames <slava.kirshman@icloud.com>
2021-10-14 12:41:26 +0300

    user REST api routes added

---
+++ pages/api/user/profile/index.ts
@@ -0,0 +1,24 @@
+import { REQUEST_METHOD_PATCH } from 'config/common';
+import { updateMyProfile } from 'db/dao/user/updateMyProfile';
+import { sendApiRouteResponse } from 'lib/sessionHelpers';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === REQUEST_METHOD_PATCH) {
+    const payload = await updateMyProfile({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  res.status(405).send({
+    success: false,
+    message: 'wrong method',
+  });
+  return;
+};

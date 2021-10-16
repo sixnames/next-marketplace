eff09b42e41aeb68d9a5fa47fb3d4f0e50700c14
sixnames <slava.kirshman@icloud.com>
2021-10-16 18:05:02 +0300

    promo spec file added

---
+++ cypress/e2e/console/promo.spec.ts
@@ -0,0 +1,12 @@
+import { ROUTE_CONSOLE } from 'config/common';
+import { fixtureIds } from 'cypress/fixtures/fixtureIds';
+
+describe('Promo', () => {
+  beforeEach(() => {
+    cy.testAuth(`${ROUTE_CONSOLE}/${fixtureIds.companyA}/promo`, 'ownerA@gmail.com');
+  });
+
+  it('Should CRUD prop', () => {
+    cy.getByCy('user-categories-list').should('exist');
+  });
+});

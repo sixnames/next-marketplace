daf837ee98e97ab114a840fbe7c2fbc5fd3791ee
sixnames <slava.kirshman@icloud.com>
2021-10-17 12:27:54 +0300

    promo CRUD api route added

---
+++ pages/api/promo/index.ts
@@ -0,0 +1,53 @@
+import { REQUEST_METHOD_DELETE, REQUEST_METHOD_PATCH, REQUEST_METHOD_POST } from 'config/common';
+import { createPromo } from 'db/dao/promo/createPromo';
+import { deletePromo } from 'db/dao/promo/deletePromo';
+import { updatePromo } from 'db/dao/promo/updatePromo';
+import { sendApiRouteResponse } from 'lib/sessionHelpers';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // create
+  if (req.method === REQUEST_METHOD_POST) {
+    const payload = await createPromo({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // update
+  if (req.method === REQUEST_METHOD_PATCH) {
+    const payload = await updatePromo({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // delete
+  if (req.method === REQUEST_METHOD_DELETE) {
+    const payload = await deletePromo({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  res.status(405).send({
+    success: false,
+    message: 'wrong method',
+  });
+  return;
+};

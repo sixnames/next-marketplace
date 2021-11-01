5da0e8beebe4ae0b0ebf93ef4d74aff7b27c387d
sixnames <slava.kirshman@icloud.com>
2021-11-01 15:51:51 +0200

    SupplierProductModel added

--- db/dbModels.ts
+++ db/dbModels.ts
@@ -295,6 +295,16 @@ export interface SupplierModel extends BaseModel, TimestampModel {
   descriptionI18n?: TranslationModel | null;
 }
 
+export interface SupplierProductModel {
+  _id: ObjectIdModel;
+  shopProductId: ObjectIdModel;
+  shopId: ObjectIdModel;
+  companyId: ObjectIdModel;
+  companySlug: ObjectIdModel;
+  price: number;
+  charge: number;
+}
+
 export interface CartProductModel {
   _id: ObjectIdModel;
   shopProductId?: ObjectIdModel | null;
@@ -793,6 +803,7 @@ export interface ShopProductModel
   available: number;
   citySlug: string;
   price: number;
+  recommendedPrice?: number | null;
   oldPrice?: number | null;
   oldPrices: ShopProductOldPriceModel[];
   discountedPercent: number;

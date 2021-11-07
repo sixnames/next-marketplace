37abf4237d279f157d5618d46ce3f0e8b38a272e
sixnames <slava.kirshman@icloud.com>
2021-11-07 10:40:20 +0200

    useProductMutations.ts and api routes added

--- hooks/mutations/useProductMutations.ts
+++ hooks/mutations/useProductMutations.ts
@@ -1,109 +1,129 @@
-/*
-export const DELETE_PRODUCT_FROM_RUBRIC_MUTATION = gql`
-  mutation DeleteProductFromRubric($input: DeleteProductFromRubricInput!) {
-    deleteProductFromRubric(input: $input) {
-      success
-      message
-    }
-  }
-`;
+import {
+  REQUEST_METHOD_DELETE,
+  REQUEST_METHOD_PATCH,
+  REQUEST_METHOD_POST,
+  ROUTE_CMS,
+} from 'config/common';
+import { CopyProductInputInterface } from 'db/dao/product/copyProduct';
+import { CreateProductInputInterface } from 'db/dao/product/createProduct';
+import { CreateProductWithSyncErrorInputInterface } from 'db/dao/product/createProductWithSyncError';
+import { DeleteProductInputInterface } from 'db/dao/product/deleteProduct';
+import { UpdateProductInputInterface } from 'db/dao/product/updateProduct';
+import { UpdateProductAssetIndexInputInterface } from 'db/dao/product/updateProductAssetIndex';
+import { UpdateProductCategoryInputInterface } from 'db/dao/product/updateProductCategory';
+import { UpdateProductCounterInputInterface } from 'db/dao/product/updateProductCounter';
+import { UpdateProductWithSyncErrorInputInterface } from 'db/dao/product/updateProductWithSyncError';
+import { ProductPayloadModel } from 'db/dbModels';
+import { ProductInterface } from 'db/uiInterfaces';
+import { useMutationHandler } from 'hooks/mutations/useFetch';
+import { useRouter } from 'next/router';
 
-export const CARD_COUNTERS_MUTATION = gql`
-  mutation UpdateProductCounter($input: UpdateProductCounterInput!) {
-    updateProductCounter(input: $input)
-  }
-`;
+const basePath = '/api/product';
 
-export const UPDATE_PRODUCT_WITH_SYNC_ERROR_MUTATION = gql`
-  mutation UpdateProductWithSyncError($input: UpdateProductWithSyncErrorInput!) {
-    updateProductWithSyncError(input: $input) {
-      success
-      message
-    }
-  }
-`;
+function getCmsProductUrl(product: ProductInterface) {
+  return `${ROUTE_CMS}/rubrics/${product.rubricId}/products/product/${product._id}`;
+}
 
-export const CREATE_PRODUCT_WITH_SYNC_ERROR_MUTATION = gql`
-  mutation CreateProductWithSyncError($input: CreateProductWithSyncErrorInput!) {
-    createProductWithSyncError(input: $input) {
-      success
-      message
-      payload {
-        _id
-        rubricId
+// create
+export const useCreateProduct = () => {
+  const router = useRouter();
+  return useMutationHandler<ProductPayloadModel, CreateProductInputInterface>({
+    path: basePath,
+    method: REQUEST_METHOD_POST,
+    onSuccess: ({ payload }) => {
+      if (payload) {
+        router.push(getCmsProductUrl(payload)).catch(console.log);
       }
-    }
-  }
-`;
+    },
+  });
+};
 
-export const UPDATE_PRODUCT_MUTATION = gql`
-  mutation UpdateProduct($input: UpdateProductInput!) {
-    updateProduct(input: $input) {
-      success
-      message
-    }
-  }
-`;
+// update
+export const useUpdateProduct = () => {
+  return useMutationHandler<ProductPayloadModel, UpdateProductInputInterface>({
+    path: basePath,
+    method: REQUEST_METHOD_PATCH,
+  });
+};
 
-export const DELETE_PRODUCT_ASSET_MUTATION = gql`
-  mutation DeleteProductAsset($input: DeleteProductAssetInput!) {
-    deleteProductAsset(input: $input) {
-      success
-      message
-    }
-  }
-`;
+// delete
+export const useDeleteProduct = () => {
+  return useMutationHandler<ProductPayloadModel, DeleteProductInputInterface>({
+    path: basePath,
+    method: REQUEST_METHOD_DELETE,
+  });
+};
 
-export const UPDATE_PRODUCT_ASSET_INDEX_MUTATION = gql`
-  mutation UpdateProductAssetIndex($input: UpdateProductAssetIndexInput!) {
-    updateProductAssetIndex(input: $input) {
-      success
-      message
-    }
-  }
-`;
-
-export const CREATE_PRODUCT_MUTATION = gql`
-  mutation CreateProduct($input: CreateProductInput!) {
-    createProduct(input: $input) {
-      success
-      message
-      payload {
-        _id
-        rubricId
+// copy
+export const useCopyProduct = () => {
+  const router = useRouter();
+  return useMutationHandler<ProductPayloadModel, CopyProductInputInterface>({
+    path: `${basePath}/copy`,
+    method: REQUEST_METHOD_POST,
+    onSuccess: ({ payload }) => {
+      if (payload) {
+        router.push(getCmsProductUrl(payload)).catch(console.log);
       }
-    }
-  }
-`;
+    },
+  });
+};
 
-export const COPY_PRODUCT_MUTATION = gql`
-  mutation CopyProduct($input: CopyProductInput!) {
-    copyProduct(input: $input) {
-      success
-      message
-      payload {
-        _id
-        rubricId
+// create with sync error
+export const useCreateProductWithSyncError = () => {
+  const router = useRouter();
+  return useMutationHandler<ProductPayloadModel, CreateProductWithSyncErrorInputInterface>({
+    path: `${basePath}/sync-error`,
+    method: REQUEST_METHOD_POST,
+    onSuccess: ({ payload }) => {
+      if (payload) {
+        router.push(getCmsProductUrl(payload)).catch(console.log);
       }
-    }
-  }
-`;
+    },
+  });
+};
 
-export const UPDATE_PRODUCT_CATEGORY_MUTATION = gql`
-  mutation UpdateProductCategory($input: UpdateProductCategoryInput!) {
-    updateProductCategory(input: $input) {
-      success
-      message
-    }
-  }
-`;
+// create with sync error
+export const useUpdateProductWithSyncError = () => {
+  const router = useRouter();
+  return useMutationHandler<ProductPayloadModel, UpdateProductWithSyncErrorInputInterface>({
+    path: `${basePath}/sync-error`,
+    method: REQUEST_METHOD_PATCH,
+    onSuccess: ({ payload }) => {
+      if (payload) {
+        router.push(getCmsProductUrl(payload)).catch(console.log);
+      }
+    },
+  });
+};
 
-export const UPDATE_PRODUCT_CATEGORY_VISIBILITY_MUTATION = gql`
-  mutation UpdateProductCategoryVisibility($input: UpdateProductCategoryInput!) {
-    updateProductCategoryVisibility(input: $input) {
-      success
-      message
-    }
-  }
-`;
-* */
+// update asset index
+export const useUpdateProductAssetIndex = () => {
+  return useMutationHandler<ProductPayloadModel, UpdateProductAssetIndexInputInterface>({
+    path: `${basePath}/asset-index`,
+    method: REQUEST_METHOD_PATCH,
+  });
+};
+
+// update category
+export const useUpdateProductCategory = () => {
+  return useMutationHandler<ProductPayloadModel, UpdateProductCategoryInputInterface>({
+    path: `${basePath}/category`,
+    method: REQUEST_METHOD_PATCH,
+  });
+};
+
+// update category visibility
+export const useUpdateProductCategoryVisibility = () => {
+  return useMutationHandler<ProductPayloadModel, UpdateProductCategoryInputInterface>({
+    path: `${basePath}/category/visibility`,
+    method: REQUEST_METHOD_PATCH,
+  });
+};
+
+// update counter
+export const useUpdateProductCounter = () => {
+  return useMutationHandler<ProductPayloadModel, UpdateProductCounterInputInterface>({
+    path: `${basePath}/counter`,
+    method: REQUEST_METHOD_PATCH,
+  });
+};

e95c9eb24dad336b275a496dc996c87ee9664a1d
sixnames <slava.kirshman@icloud.com>
2021-11-08 10:13:31 +0200

    ConsoleRubricProductAssets component added

--- pages/cms/companies/[companyId]/rubrics/[rubricId]/products/product/[productId]/assets.tsx
+++ pages/cms/companies/[companyId]/rubrics/[rubricId]/products/product/[productId]/assets.tsx
@@ -1,44 +1,61 @@
 import ConsoleRubricProductAssets from 'components/console/ConsoleRubricProductAssets';
-import { DEFAULT_COMPANY_SLUG, ROUTE_CMS } from 'config/common';
-import { ProductInterface } from 'db/uiInterfaces';
+import { ROUTE_CMS } from 'config/common';
+import { COL_COMPANIES } from 'db/collectionNames';
+import { getDatabase } from 'db/mongodb';
+import { CompanyInterface, ProductInterface } from 'db/uiInterfaces';
 import { AppContentWrapperBreadCrumbs } from 'layout/AppContentWrapper';
 import CmsLayout from 'layout/cms/CmsLayout';
 import CmsProductLayout from 'layout/cms/CmsProductLayout';
 import { getCmsProduct } from 'lib/productUtils';
 import { castDbData, getAppInitialData } from 'lib/ssrUtils';
+import { ObjectId } from 'mongodb';
 import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next';
 import { PagePropsInterface } from 'pages/_app';
 import * as React from 'react';
 
 interface ProductAssetsInterface {
   product: ProductInterface;
+  currentCompany?: CompanyInterface | null;
+  routeBasePath: string;
 }
 
-const ProductAssets: React.FC<ProductAssetsInterface> = ({ product }) => {
+const ProductAssets: React.FC<ProductAssetsInterface> = ({
+  product,
+  currentCompany,
+  routeBasePath,
+}) => {
   const breadcrumbs: AppContentWrapperBreadCrumbs = {
     currentPageName: 'Изображения',
     config: [
       {
-        name: 'Рубрикатор',
-        href: `${ROUTE_CMS}/rubrics`,
+        name: 'Компании',
+        href: `${ROUTE_CMS}/companies`,
+      },
+      {
+        name: `${currentCompany?.name}`,
+        href: routeBasePath,
+      },
+      {
+        name: `Рубрикатор`,
+        href: `${routeBasePath}/rubrics`,
       },
       {
         name: `${product.rubric?.name}`,
-        href: `${ROUTE_CMS}/rubrics/${product.rubric?._id}`,
+        href: `${routeBasePath}/rubrics/${product.rubric?._id}`,
       },
       {
         name: `Товары`,
-        href: `${ROUTE_CMS}/rubrics/${product.rubric?._id}/products/${product.rubric?._id}`,
+        href: `${routeBasePath}/rubrics/${product.rubric?._id}/products/${product.rubric?._id}`,
       },
       {
-        name: `${product.cardTitle}`,
-        href: `${ROUTE_CMS}/rubrics/${product.rubric?._id}/products/product/${product._id}`,
+        name: `${product.snippetTitle}`,
+        href: `${routeBasePath}/rubrics/${product.rubric?._id}/products/product/${product._id}`,
       },
     ],
   };
 
   return (
-    <CmsProductLayout product={product} breadcrumbs={breadcrumbs}>
+    <CmsProductLayout product={product} breadcrumbs={breadcrumbs} basePath={routeBasePath}>
       <ConsoleRubricProductAssets product={product} />
     </CmsProductLayout>
   );
@@ -59,6 +76,8 @@ export const getServerSideProps = async (
 ): Promise<GetServerSidePropsResult<ProductPageInterface>> => {
   const { query } = context;
   const { productId, rubricId } = query;
+  const { db } = await getDatabase();
+  const companiesCollection = db.collection<CompanyInterface>(COL_COMPANIES);
   const { props } = await getAppInitialData({ context });
   if (!props || !productId || !rubricId) {
     return {
@@ -66,10 +85,29 @@ export const getServerSideProps = async (
     };
   }
 
+  // get company
+  const companyId = new ObjectId(`${query.companyId}`);
+  const companyAggregationResult = await companiesCollection
+    .aggregate([
+      {
+        $match: {
+          _id: companyId,
+        },
+      },
+    ])
+    .toArray();
+  const companyResult = companyAggregationResult[0];
+  if (!companyResult) {
+    return {
+      notFound: true,
+    };
+  }
+  const companySlug = companyResult.slug;
+
   const payload = await getCmsProduct({
     locale: props.sessionLocale,
     productId: `${productId}`,
-    companySlug: DEFAULT_COMPANY_SLUG,
+    companySlug,
   });
 
   if (!payload) {
@@ -82,6 +120,8 @@ export const getServerSideProps = async (
     props: {
       ...props,
       product: castDbData(payload.product),
+      currentCompany: castDbData(companyResult),
+      routeBasePath: `${ROUTE_CMS}/companies/${companyResult._id}`,
     },
   };
 };

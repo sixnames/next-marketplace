0a09aa84fd83ef22019eee02e2ab234ba73f8494
sixnames <slava.kirshman@icloud.com>
2021-11-15 18:12:04 +0200

    card redirect added

---
+++ pages/[companySlug]/[card].tsx
@@ -0,0 +1,24 @@
+import { getSiteInitialData } from 'lib/ssrUtils';
+import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next';
+import * as React from 'react';
+
+const CardPage: NextPage = () => {
+  return <div />;
+};
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<any>> {
+  const { props } = await getSiteInitialData({
+    context,
+  });
+
+  return {
+    redirect: {
+      destination: `${props.urlPrefix}/${context.query?.card}`,
+      permanent: true,
+    },
+  };
+}
+
+export default CardPage;

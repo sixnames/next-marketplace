c32c102fdcacd656167f5b3893f1c80e8af6a079
sixnames <slava.kirshman@icloud.com>
2021-12-03 08:38:58 +0200

    Header contacts block updated

--- layout/header/Header.tsx
+++ layout/header/Header.tsx
@@ -582,7 +582,7 @@ const Header: React.FC<HeaderInterface> = ({ headerPageGroups, currentRubricSlug
                       <div>{domainCompany.mainShop.address.formattedAddress}</div>
                     </div>
 
-                    <div className='flex items-center gap-3 mt-4'>
+                    <div className='flex items-center gap-3 mt-2'>
                       <div className='text-theme'>
                         <Icon name={'phone'} className='w-5 h-5' />
                       </div>

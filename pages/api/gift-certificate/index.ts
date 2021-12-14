dd1c916c5fa3db770e77f3cdaa67eb1dab828ae1
sixnames <slava.kirshman@icloud.com>
2021-12-14 18:43:22 +0200

    gift certificate api routes added

---
+++ pages/api/gift-certificate/index.ts
@@ -0,0 +1,53 @@
+import { REQUEST_METHOD_DELETE, REQUEST_METHOD_PATCH, REQUEST_METHOD_POST } from 'config/common';
+import { createGiftCertificate } from 'db/dao/giftCertificate/createGiftCertificate';
+import { deleteGiftCertificate } from 'db/dao/giftCertificate/deleteGiftCertificate';
+import { updateGiftCertificate } from 'db/dao/giftCertificate/updateGiftCertificate';
+import { sendApiRouteResponse } from 'lib/sessionHelpers';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // create
+  if (req.method === REQUEST_METHOD_POST) {
+    const payload = await createGiftCertificate({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // update
+  if (req.method === REQUEST_METHOD_PATCH) {
+    const payload = await updateGiftCertificate({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // delete
+  if (req.method === REQUEST_METHOD_DELETE) {
+    const payload = await deleteGiftCertificate({
+      context: { req, res },
+      input: JSON.parse(req.body),
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  res.status(405).send({
+    success: false,
+    message: 'wrong method',
+  });
+  return;
+};

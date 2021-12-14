338d6a4dffc5b98d310c44e074d8d0cffe9cf471
sixnames <slava.kirshman@icloud.com>
2021-12-14 16:57:28 +0200

    checkGiftCertificateAvailability dao util added

---
+++ db/dao/giftCertificate/checkGiftCertificateAvailability.ts
@@ -0,0 +1,109 @@
+import { COL_GIFT_CERTIFICATES } from 'db/collectionNames';
+import { GiftCertificateModel, GiftCertificatePayloadModel } from 'db/dbModels';
+import { getDatabase } from 'db/mongodb';
+import { DaoPropsInterface } from 'db/uiInterfaces';
+import getResolverErrorMessage from 'lib/getResolverErrorMessage';
+import { getRequestParams } from 'lib/sessionHelpers';
+import { ObjectId } from 'mongodb';
+
+export interface CheckGiftCertificateAvailabilityInputInterface {
+  giftCertificateId: string;
+  userId?: string | null;
+}
+
+export async function checkGiftCertificateAvailability({
+  input,
+  context,
+}: DaoPropsInterface<CheckGiftCertificateAvailabilityInputInterface>): Promise<GiftCertificatePayloadModel> {
+  try {
+    const { getApiMessage } = await getRequestParams(context);
+    const { db } = await getDatabase();
+    const giftCertificatesCollection = db.collection<GiftCertificateModel>(COL_GIFT_CERTIFICATES);
+
+    // check input
+    if (!input) {
+      return {
+        success: false,
+        message: await getApiMessage('giftCertificate.update.error'),
+      };
+    }
+
+    // get certificate
+    const giftCertificate = await giftCertificatesCollection.findOne({
+      _id: new ObjectId(input.giftCertificateId),
+    });
+    if (!giftCertificate) {
+      return {
+        success: false,
+        message: await getApiMessage('giftCertificate.update.error'),
+      };
+    }
+
+    // if user is unauthenticated
+    if (!input.userId) {
+      if (giftCertificate.userId) {
+        return {
+          success: false,
+          message: await getApiMessage('giftCertificate.update.error'),
+        };
+      }
+
+      return {
+        success: true,
+        message: await getApiMessage('giftCertificate.update.success'),
+      };
+    }
+
+    const userId = new ObjectId(input.userId);
+
+    // if new certificate
+    if (!giftCertificate.userId) {
+      // update
+      const updatedGiftCertificateResult = await giftCertificatesCollection.findOneAndUpdate(
+        {
+          _id: giftCertificate._id,
+        },
+        {
+          $set: {
+            userId,
+            updatedAt: new Date(),
+          },
+        },
+        {
+          returnDocument: 'after',
+        },
+      );
+      const updatedGiftCertificate = updatedGiftCertificateResult.value;
+      if (!updatedGiftCertificateResult.ok || !updatedGiftCertificate) {
+        return {
+          success: false,
+          message: await getApiMessage('giftCertificate.update.error'),
+        };
+      }
+
+      return {
+        success: true,
+        message: await getApiMessage('giftCertificate.update.success'),
+      };
+    }
+
+    // check if user is owner of certificate
+    if (!giftCertificate.userId.equals(userId)) {
+      return {
+        success: false,
+        message: await getApiMessage('giftCertificate.update.error'),
+      };
+    }
+
+    return {
+      success: true,
+      message: await getApiMessage('giftCertificate.update.success'),
+    };
+  } catch (e) {
+    console.log('checkGiftCertificateAvailability error ', e);
+    return {
+      success: false,
+      message: getResolverErrorMessage(e),
+    };
+  }
+}

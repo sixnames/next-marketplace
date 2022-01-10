602ad0a677920513214828c7b7050086f1e7750b
sixnames <slava.kirshman@icloud.com>
2022-01-10 14:42:23 +0200

    ts error removed

--- tests/data/blackListProducts/blackListProducts.ts
+++ tests/data/blackListProducts/blackListProducts.ts
@@ -1,6 +1,5 @@
 import { BlackListProductModel } from '../../../db/dbModels';
 import { getObjectId } from 'mongo-seeding';
-import { ObjectId } from 'mongodb';
 
 require('dotenv').config();
 
@@ -8,7 +7,7 @@ const blackListProducts: BlackListProductModel[] = [
   {
     _id: getObjectId('blacklist product 1'),
     shopId: getObjectId('shop Shop A'),
-    shopProductId: new ObjectId('a3d4fbd7a08f5238dec045b4'),
+    shopProductUid: '1',
     products: [
       {
         available: 10,

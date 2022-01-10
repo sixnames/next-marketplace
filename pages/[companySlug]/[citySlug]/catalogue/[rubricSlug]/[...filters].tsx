de4233289a535e6e3cb955080a3910a42e98eac1
sixnames <slava.kirshman@icloud.com>
2022-01-10 18:48:27 +0200

    catalogue cache removed

--- pages/[companySlug]/[citySlug]/catalogue/[rubricSlug]/[...filters].tsx
+++ pages/[companySlug]/[citySlug]/catalogue/[rubricSlug]/[...filters].tsx
@@ -1,14 +1,13 @@
-import { GetStaticPathsResult, GetStaticPropsResult, NextPage } from 'next';
+import { NextPage } from 'next';
 import * as React from 'react';
 import Catalogue, { CatalogueInterface } from '../../../../../components/Catalogue';
-import { getCatalogueIsrProps } from '../../../../../lib/catalogueIsrUtils';
-import { IsrContextInterface } from '../../../../../lib/isrUtils';
+import { getCatalogueProps } from '../../../../../lib/catalogueUtils';
 
 const CataloguePage: NextPage<CatalogueInterface> = (props) => {
   return <Catalogue {...props} />;
 };
 
-export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+/*export async function getStaticPaths(): Promise<GetStaticPathsResult> {
   const paths: any[] = [];
   return {
     paths,
@@ -20,6 +19,7 @@ export async function getStaticProps(
   context: IsrContextInterface,
 ): Promise<GetStaticPropsResult<CatalogueInterface>> {
   return getCatalogueIsrProps(context);
-}
+}*/
 
+export const getServerSideProps = getCatalogueProps;
 export default CataloguePage;

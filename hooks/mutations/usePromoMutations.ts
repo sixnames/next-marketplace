e77c1ae3ccb6f121b507752ab397970523b819d3
sixnames <slava.kirshman@icloud.com>
2022-01-17 16:54:38 +0200

    promo code mutation hooks added

--- hooks/mutations/usePromoMutations.ts
+++ hooks/mutations/usePromoMutations.ts
@@ -5,10 +5,13 @@ import {
 } from '../../config/common';
 import { AddPromoProductsInputInterface } from '../../db/dao/promo/addPromoProducts';
 import { CreatePromoInputInterface } from '../../db/dao/promo/createPromo';
+import { CreatePromoCodeInputInterface } from '../../db/dao/promo/createPromoCode';
 import { DeletePromoInputInterface } from '../../db/dao/promo/deletePromo';
+import { DeletePromoCodeInputInterface } from '../../db/dao/promo/deletePromoCode';
 import { DeletePromoProductsInputInterface } from '../../db/dao/promo/deletePromoProducts';
 import { UpdatePromoInputInterface } from '../../db/dao/promo/updatePromo';
-import { PromoPayloadModel } from '../../db/dbModels';
+import { UpdatePromoCodeInputInterface } from '../../db/dao/promo/updatePromoCode';
+import { PromoCodePayloadModel, PromoPayloadModel } from '../../db/dbModels';
 import { useMutationHandler } from './useFetch';
 
 const basePath = '/api/promo';
@@ -51,6 +54,31 @@ export const useAddPromoProducts = () => {
 export const useDeletePromoProducts = () => {
   return useMutationHandler<PromoPayloadModel, DeletePromoProductsInputInterface>({
     path: `${basePath}/products`,
+    method: REQUEST_METHOD_DELETE,
+  });
+};
+
+// promo code
+// create
+export const useCreatePromoCode = () => {
+  return useMutationHandler<PromoCodePayloadModel, CreatePromoCodeInputInterface>({
+    path: basePath,
+    method: REQUEST_METHOD_POST,
+  });
+};
+
+// update
+export const useUpdatePromoCode = () => {
+  return useMutationHandler<PromoCodePayloadModel, UpdatePromoCodeInputInterface>({
+    path: basePath,
+    method: REQUEST_METHOD_PATCH,
+  });
+};
+
+// delete
+export const useDeletePromoCode = () => {
+  return useMutationHandler<PromoCodePayloadModel, DeletePromoCodeInputInterface>({
+    path: basePath,
     method: REQUEST_METHOD_DELETE,
   });
 };

a7f0d07d29a6994ea3fd24cc6ae9493a0727bbfc
sixnames <slava.kirshman@icloud.com>
2022-01-30 16:02:42 +0200

    cms root TaskVariantsListPage

---
+++ components/console/ConsoleTaskVariantsList.tsx
@@ -0,0 +1,108 @@
+import { useRouter } from 'next/router';
+import * as React from 'react';
+import { CONFIRM_MODAL } from '../../config/modalVariants';
+import { useAppContext } from '../../context/appContext';
+import { TaskVariantInterface } from '../../db/uiInterfaces';
+import { useDeleteTaskVariant } from '../../hooks/mutations/useTaskMutations';
+import { getConsoleTaskVariantLinks } from '../../lib/linkUtils';
+import ContentItemControls from '../button/ContentItemControls';
+import FixedButtons from '../button/FixedButtons';
+import WpButton from '../button/WpButton';
+import WpLink from '../Link/WpLink';
+import { ConfirmModalInterface } from '../Modal/ConfirmModal';
+import WpTable, { WpTableColumn } from '../WpTable';
+
+export interface ConsoleTaskVariantsListInterface {
+  taskVariants: TaskVariantInterface[];
+  basePath: string;
+}
+
+const ConsoleTaskVariantsList: React.FC<ConsoleTaskVariantsListInterface> = ({
+  basePath,
+  taskVariants,
+}) => {
+  const router = useRouter();
+  const { showModal } = useAppContext();
+  const [deleteTaskVariantMutation] = useDeleteTaskVariant();
+  const links = getConsoleTaskVariantLinks({
+    basePath,
+  });
+
+  const columns: WpTableColumn<TaskVariantInterface>[] = [
+    {
+      headTitle: 'Название',
+      render: ({ dataItem }) => {
+        const links = getConsoleTaskVariantLinks({
+          basePath,
+          taskVariantId: dataItem._id,
+        });
+        return <WpLink href={links.root}>{dataItem.name}</WpLink>;
+      },
+    },
+    {
+      render: ({ dataItem }) => {
+        const links = getConsoleTaskVariantLinks({
+          basePath,
+          taskVariantId: dataItem._id,
+        });
+        return (
+          <div className='flex justify-end'>
+            <ContentItemControls
+              testId={`${dataItem.name}`}
+              updateTitle={'Редактировать тип задачи'}
+              updateHandler={() => {
+                router.push(links.root).catch(console.log);
+              }}
+              deleteTitle={'Удалить тип задачи'}
+              deleteHandler={() => {
+                showModal<ConfirmModalInterface>({
+                  variant: CONFIRM_MODAL,
+                  props: {
+                    message: `Вы уверенны, что хотите удалить тип задачи ${dataItem.name}?`,
+                    confirm: () => {
+                      deleteTaskVariantMutation({
+                        _id: `${dataItem._id}`,
+                      }).catch(console.log);
+                    },
+                  },
+                });
+              }}
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className='relative' data-cy={'task-variants-list'}>
+      <div className='overflow-x-auto overflow-y-hidden'>
+        <WpTable<TaskVariantInterface>
+          testIdKey={'name'}
+          columns={columns}
+          data={taskVariants}
+          onRowDoubleClick={(dataItem) => {
+            const links = getConsoleTaskVariantLinks({
+              basePath,
+              taskVariantId: dataItem._id,
+            });
+            router.push(links.root).catch(console.log);
+          }}
+        />
+      </div>
+      <FixedButtons>
+        <WpButton
+          size='small'
+          frameClassName='w-auto'
+          onClick={() => {
+            router.push(links.create).catch(console.log);
+          }}
+        >
+          Создать тип задачи
+        </WpButton>
+      </FixedButtons>
+    </div>
+  );
+};
+
+export default ConsoleTaskVariantsList;

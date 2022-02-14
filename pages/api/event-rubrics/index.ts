6f271f1d2cf5837be6f004d2f9fbcfcb04b424a0
sixnames <slava.kirshman@icloud.com>
2022-02-14 14:56:52 +0200

    event rubric api routes added

---
+++ pages/api/event-rubrics/index.ts
@@ -0,0 +1,65 @@
+import { createEventRubric } from 'db/dao/eventRubrics/createEventRubric';
+import { deleteEventRubric } from 'db/dao/eventRubrics/deleteEventRubric';
+import { getEventRubricsList } from 'db/dao/eventRubrics/getEventRubricsList';
+import { updateEventRubric } from 'db/dao/eventRubrics/updateEventRubric';
+import { sendApiRouteResponse } from 'lib/sessionHelpers';
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  REQUEST_METHOD_DELETE,
+  REQUEST_METHOD_GET,
+  REQUEST_METHOD_PATCH,
+  REQUEST_METHOD_POST,
+} from 'config/common';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // create
+  if (req.method === REQUEST_METHOD_POST) {
+    const payload = await createEventRubric({
+      context: { req, res },
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // update
+  if (req.method === REQUEST_METHOD_PATCH) {
+    const payload = await updateEventRubric({
+      context: { req, res },
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // delete
+  if (req.method === REQUEST_METHOD_DELETE) {
+    const payload = await deleteEventRubric({
+      context: { req, res },
+    });
+    sendApiRouteResponse({
+      payload,
+      res,
+    });
+    return;
+  }
+
+  // get list of rubrics
+  if (req.method === REQUEST_METHOD_GET) {
+    const payload = await getEventRubricsList({
+      context: { req, res },
+    });
+    res.status(200).send(payload);
+    return;
+  }
+
+  res.status(405).send({
+    success: false,
+    message: 'wrong method',
+  });
+  return;
+};

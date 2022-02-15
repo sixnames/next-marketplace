14ef652a18c5406c37de6b045b5b1e184b0a2db1
sixnames <slava.kirshman@icloud.com>
2022-02-15 18:51:39 +0200

    updateEventNumberAttribute dao util added

---
+++ db/dao/events/updateEventSelectAttribute.ts
@@ -0,0 +1,325 @@
+import { getTaskVariantSlugByRule } from 'lib/config/constantSelects';
+import { getFullProductSummaryWithDraft } from 'lib/productUtils';
+import { addTaskLogItem, findOrCreateUserTask } from 'db/dao/tasks/taskUtils';
+import { ObjectId } from 'mongodb';
+import { DEFAULT_COMPANY_SLUG, FILTER_SEPARATOR, TASK_STATE_IN_PROGRESS } from 'lib/config/common';
+import getResolverErrorMessage from 'lib/getResolverErrorMessage';
+import { getAttributeReadableValueLocales } from 'lib/productAttributesUtils';
+import { getOperationPermission, getRequestParams } from 'lib/sessionHelpers';
+import { getParentTreeIds } from 'lib/treeUtils';
+import { execUpdateProductTitles } from 'lib/updateProductTitles';
+import { COL_OPTIONS } from 'db/collectionNames';
+import { ObjectIdModel, ProductPayloadModel, SummaryDiffModel } from 'db/dbModels';
+import { getDbCollections } from 'db/mongodb';
+import { DaoPropsInterface, ProductAttributeInterface } from 'db/uiInterfaces';
+
+export interface UpdateEventSelectAttributeInputInterface {
+  productId: string;
+  taskId?: string | null;
+  productAttributeId: string;
+  attributeId: string;
+  selectedOptionsIds: string[];
+}
+
+export async function updateEventSelectAttribute({
+  context,
+  input,
+}: DaoPropsInterface<UpdateEventSelectAttributeInputInterface>): Promise<ProductPayloadModel> {
+  const { getApiMessage, locale } = await getRequestParams(context);
+  const collections = await getDbCollections();
+  const productSummariesCollection = collections.productSummariesCollection();
+  const productFacetsCollection = collections.productFacetsCollection();
+  const shopProductsCollection = collections.shopProductsCollection();
+  const attributesCollection = collections.attributesCollection();
+  const optionsCollection = collections.optionsCollection();
+
+  const session = collections.client.startSession();
+
+  let mutationPayload: ProductPayloadModel = {
+    success: false,
+    message: await getApiMessage('products.update.error'),
+  };
+
+  try {
+    await session.withTransaction(async () => {
+      // permission
+      const { allow, message, role, user } = await getOperationPermission({
+        context,
+        slug: 'updateProductAttributes',
+      });
+      if (!allow) {
+        mutationPayload = {
+          success: false,
+          message,
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      // check input
+      if (!input) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.error'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      const selectedOptionsIds = input.selectedOptionsIds.map((_id) => new ObjectId(_id));
+      const attributeId = new ObjectId(input.attributeId);
+      const productAttributeId = new ObjectId(input.productAttributeId);
+
+      // get summary or summary draft
+      const taskVariantSlug = getTaskVariantSlugByRule('updateProductAttributes');
+      const summaryPayload = await getFullProductSummaryWithDraft({
+        locale,
+        taskId: input.taskId,
+        productId: input.productId,
+        companySlug: DEFAULT_COMPANY_SLUG,
+        isContentManager: role.isContentManager,
+      });
+      if (!summaryPayload) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.notFound'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+      const { summary } = summaryPayload;
+      const updatedSummary = { ...summary };
+      const diff: SummaryDiffModel = {};
+
+      // get product attribute
+      let productAttribute = summary.attributes.find((productAttribute) => {
+        return productAttribute.attributeId.equals(attributeId);
+      });
+      const productAttributeNotExist = !productAttribute;
+
+      // create new product attribute if original is absent
+      if (!productAttribute) {
+        productAttribute = {
+          _id: productAttributeId,
+          attributeId,
+          optionIds: [],
+          filterSlugs: [],
+          number: undefined,
+          textI18n: {},
+          readableValueI18n: {},
+        };
+      }
+
+      // get attribute
+      const attribute = await attributesCollection.findOne({
+        _id: attributeId,
+      });
+      if (!attribute) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.error'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      // update product attribute
+      // get selected options tree
+      const finalOptionIds: ObjectIdModel[] = [];
+      for await (const optionId of selectedOptionsIds) {
+        const optionsTreeIds = await getParentTreeIds({
+          collectionName: COL_OPTIONS,
+          _id: optionId,
+          acc: [],
+        });
+        optionsTreeIds.forEach((_id) => finalOptionIds.push(_id));
+      }
+      const finalOptions = await optionsCollection
+        .find({
+          _id: {
+            $in: finalOptionIds,
+          },
+        })
+        .toArray();
+      const finalFilterSlugs = finalOptions.map(
+        ({ slug }) => `${attribute.slug}${FILTER_SEPARATOR}${slug}`,
+      );
+      const oldFilterSlugs = [...productAttribute.filterSlugs];
+      const readableValueI18n = getAttributeReadableValueLocales({
+        productAttribute: {
+          ...productAttribute,
+          attribute: {
+            ...attribute,
+            options: finalOptions,
+          },
+        },
+        gender: summary.gender,
+      });
+      productAttribute.readableValueI18n = readableValueI18n;
+      productAttribute.optionIds = finalOptionIds;
+      productAttribute.filterSlugs = finalFilterSlugs;
+
+      // remove attribute if value is empty
+      if (finalOptionIds.length < 1) {
+        updatedSummary.attributes = updatedSummary.attributes.filter((productAttribute) => {
+          return !productAttribute.attributeId.equals(attributeId);
+        });
+        updatedSummary.attributeIds = updatedSummary.attributeIds.filter((existingAttributeId) => {
+          return !existingAttributeId.equals(attributeId);
+        });
+        updatedSummary.filterSlugs = updatedSummary.filterSlugs.filter((filterSlug) => {
+          return !oldFilterSlugs.includes(filterSlug);
+        });
+        diff.deleted = {
+          selectAttributes: [productAttribute._id],
+        };
+      } else {
+        // add new attribute
+        if (productAttributeNotExist) {
+          updatedSummary.attributeIds.push(attributeId);
+          updatedSummary.attributes.push(productAttribute);
+          finalFilterSlugs.forEach((filterSlug) => {
+            updatedSummary.filterSlugs.push(filterSlug);
+          });
+          diff.added = {
+            selectAttributes: [productAttribute._id],
+          };
+        } else {
+          // update existing attribute
+          updatedSummary.attributes = updatedSummary.attributes.reduce(
+            (acc: ProductAttributeInterface[], prevProductAttribute) => {
+              if (prevProductAttribute.attributeId.equals(attributeId) && productAttribute) {
+                return [...acc, productAttribute];
+              }
+              return [...acc, prevProductAttribute];
+            },
+            [],
+          );
+          updatedSummary.filterSlugs = updatedSummary.filterSlugs.filter((filterSlug) => {
+            return !oldFilterSlugs.includes(filterSlug);
+          });
+          finalFilterSlugs.forEach((filterSlug) => {
+            updatedSummary.filterSlugs.push(filterSlug);
+          });
+          diff.updated = {
+            selectAttributes: [productAttribute._id],
+          };
+        }
+      }
+
+      // create task log for content manager
+      if (role.isContentManager && user) {
+        const task = await findOrCreateUserTask({
+          productId: summary._id,
+          variantSlug: taskVariantSlug,
+          executorId: user._id,
+          taskId: input.taskId,
+        });
+
+        if (!task) {
+          mutationPayload = {
+            success: false,
+            message: await getApiMessage('tasks.create.error'),
+          };
+          await session.abortTransaction();
+          return;
+        }
+
+        const newTaskLogResult = await addTaskLogItem({
+          taskId: task._id,
+          diff,
+          prevStateEnum: task.stateEnum,
+          nextStateEnum: TASK_STATE_IN_PROGRESS,
+          draft: updatedSummary,
+          createdById: user._id,
+        });
+        if (!newTaskLogResult) {
+          mutationPayload = {
+            success: false,
+            message: await getApiMessage('products.update.error'),
+          };
+          await session.abortTransaction();
+          return;
+        }
+
+        mutationPayload = {
+          success: true,
+          message: await getApiMessage('products.update.success'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      // update documents
+      const updatedProductAttributeResult = await productSummariesCollection.findOneAndUpdate(
+        {
+          _id: summary._id,
+        },
+        {
+          $set: {
+            filterSlugs: updatedSummary.filterSlugs,
+            attributeIds: updatedSummary.attributeIds,
+            attributes: updatedSummary.attributes.map((productAttribute) => {
+              return {
+                _id: productAttribute._id,
+                attributeId: productAttribute.attributeId,
+                filterSlugs: productAttribute.filterSlugs,
+                optionIds: productAttribute.optionIds,
+                readableValueI18n: productAttribute.readableValueI18n,
+                number: productAttribute.number,
+                textI18n: productAttribute.textI18n,
+              };
+            }),
+          },
+        },
+      );
+      if (!updatedProductAttributeResult.ok) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.error'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+      await productFacetsCollection.findOneAndUpdate(
+        {
+          _id: summary._id,
+        },
+        {
+          $set: {
+            filterSlugs: updatedSummary.filterSlugs,
+            attributeIds: updatedSummary.attributeIds,
+          },
+        },
+      );
+      await shopProductsCollection.updateMany(
+        {
+          productId: summary._id,
+        },
+        {
+          $set: {
+            filterSlugs: updatedSummary.filterSlugs,
+          },
+        },
+      );
+
+      // update product title
+      execUpdateProductTitles(`productId=${summary._id.toHexString()}`);
+      mutationPayload = {
+        success: true,
+        message: await getApiMessage('products.update.success'),
+        payload: summary,
+      };
+    });
+
+    return mutationPayload;
+  } catch (e) {
+    console.log('updateEventSelectAttribute', e);
+    return {
+      success: false,
+      message: getResolverErrorMessage(e),
+    };
+  } finally {
+    await session.endSession();
+  }
+}
---
+++ db/dao/events/updateEventTextAttribute.ts
@@ -0,0 +1,292 @@
+import { getTaskVariantSlugByRule } from 'lib/config/constantSelects';
+import { addTaskLogItem, findOrCreateUserTask } from 'db/dao/tasks/taskUtils';
+import { getFullProductSummaryWithDraft } from 'lib/productUtils';
+import { ObjectId } from 'mongodb';
+import {
+  DEFAULT_COMPANY_SLUG,
+  DEFAULT_LOCALE,
+  SECONDARY_LOCALE,
+  TASK_STATE_IN_PROGRESS,
+} from 'lib/config/common';
+import getResolverErrorMessage from 'lib/getResolverErrorMessage';
+import { getAttributeReadableValueLocales } from 'lib/productAttributesUtils';
+import { getOperationPermission, getRequestParams } from 'lib/sessionHelpers';
+import { ProductPayloadModel, SummaryDiffModel, TranslationModel } from 'db/dbModels';
+import { getDbCollections } from 'db/mongodb';
+import {
+  DaoPropsInterface,
+  ProductAttributeInterface,
+  ProductSummaryInterface,
+} from 'db/uiInterfaces';
+
+export interface UpdateEventTextAttributeItemInputInterface {
+  productAttributeId: string;
+  attributeId: string;
+  textI18n?: TranslationModel | null;
+}
+
+export interface UpdateEventTextAttributeInputInterface {
+  productId: string;
+  attributes: UpdateEventTextAttributeItemInputInterface[];
+  taskId?: string | null;
+}
+
+export async function updateEventTextAttribute({
+  input,
+  context,
+}: DaoPropsInterface<UpdateEventTextAttributeInputInterface>): Promise<ProductPayloadModel> {
+  const { getApiMessage, locale } = await getRequestParams(context);
+  const collections = await getDbCollections();
+  const productSummariesCollection = collections.productSummariesCollection();
+  const productFacetsCollection = collections.productFacetsCollection();
+  const attributesCollection = collections.attributesCollection();
+
+  const session = collections.client.startSession();
+
+  let mutationPayload: ProductPayloadModel = {
+    success: false,
+    message: await getApiMessage('products.update.error'),
+  };
+
+  try {
+    await session.withTransaction(async () => {
+      // permission
+      const { allow, message, role, user } = await getOperationPermission({
+        context,
+        slug: 'updateProductAttributes',
+      });
+      if (!allow) {
+        mutationPayload = {
+          success: false,
+          message,
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      // check input
+      if (!input) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.error'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      // get summary or summary draft
+      const diff: SummaryDiffModel = {
+        added: {
+          textAttributes: [],
+        },
+        updated: {
+          textAttributes: [],
+        },
+        deleted: {
+          textAttributes: [],
+        },
+      };
+      const taskVariantSlug = getTaskVariantSlugByRule('updateProductAttributes');
+      const summaryPayload = await getFullProductSummaryWithDraft({
+        locale,
+        productId: input.productId,
+        taskId: input.taskId,
+        companySlug: DEFAULT_COMPANY_SLUG,
+        isContentManager: role.isContentManager,
+      });
+      if (!summaryPayload) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.error'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      const { summary } = summaryPayload;
+      let productAttributes = summary.attributes;
+      let attributeIds = summary.attributeIds;
+
+      for await (const inputAttribute of input.attributes) {
+        const { textI18n } = inputAttribute;
+        const productAttributeId = new ObjectId(inputAttribute.productAttributeId);
+        const attributeId = new ObjectId(inputAttribute.attributeId);
+
+        // remove attribute if value not set
+        if (!textI18n || !textI18n[DEFAULT_LOCALE]) {
+          productAttributes = productAttributes.filter((productAttribute) => {
+            return !productAttribute.attributeId.equals(attributeId);
+          });
+          attributeIds = attributeIds.filter((_id) => {
+            return !_id.equals(attributeId);
+          });
+          diff.deleted?.textAttributes?.push(productAttributeId);
+          continue;
+        }
+
+        // get product attribute
+        let productAttribute = await summary.attributes.find(({ _id }) => {
+          return _id.equals(productAttributeId);
+        });
+
+        // get attribute
+        const attribute = await attributesCollection.findOne({ _id: attributeId });
+        if (!attribute) {
+          continue;
+        }
+        const productAttributeNotExist = !productAttribute;
+
+        // create new product attribute if original is absent
+        if (!productAttribute) {
+          productAttribute = {
+            _id: productAttributeId,
+            attributeId,
+            optionIds: [],
+            filterSlugs: [],
+            number: null,
+            textI18n,
+            readableValueI18n: {},
+          };
+        }
+        const readableValueI18n = getAttributeReadableValueLocales({
+          productAttribute: {
+            ...productAttribute,
+            attribute,
+          },
+          gender: summary.gender,
+        });
+        productAttribute.readableValueI18n = readableValueI18n;
+
+        // add new attribute
+        if (productAttributeNotExist) {
+          productAttributes.push(productAttribute);
+          attributeIds.push(attributeId);
+          diff.added?.textAttributes?.push(productAttributeId);
+          continue;
+        }
+
+        if (
+          productAttribute.textI18n?.[DEFAULT_LOCALE] !== textI18n[DEFAULT_LOCALE] ||
+          productAttribute.textI18n?.[SECONDARY_LOCALE] !== textI18n[SECONDARY_LOCALE]
+        ) {
+          productAttribute.textI18n = textI18n;
+          // update existing attribute
+          productAttributes = productAttributes.reduce(
+            (acc: ProductAttributeInterface[], prevProductAttribute) => {
+              if (prevProductAttribute._id.equals(productAttributeId) && productAttribute) {
+                return [...acc, productAttribute];
+              }
+              return [...acc, prevProductAttribute];
+            },
+            [],
+          );
+          diff.updated?.textAttributes?.push(productAttributeId);
+        }
+      }
+
+      // create task log for content manager
+      const updatedSummary: ProductSummaryInterface = {
+        ...summary,
+        attributeIds,
+        attributes: productAttributes,
+      };
+      if (role.isContentManager && user) {
+        const task = await findOrCreateUserTask({
+          productId: summary._id,
+          variantSlug: taskVariantSlug,
+          executorId: user._id,
+          taskId: input.taskId,
+        });
+
+        if (!task) {
+          mutationPayload = {
+            success: false,
+            message: await getApiMessage('tasks.create.error'),
+          };
+          await session.abortTransaction();
+          return;
+        }
+
+        const newTaskLogResult = await addTaskLogItem({
+          taskId: task._id,
+          diff,
+          prevStateEnum: task.stateEnum,
+          nextStateEnum: TASK_STATE_IN_PROGRESS,
+          draft: updatedSummary,
+          createdById: user._id,
+        });
+        if (!newTaskLogResult) {
+          mutationPayload = {
+            success: false,
+            message: await getApiMessage('products.update.error'),
+          };
+          await session.abortTransaction();
+          return;
+        }
+
+        mutationPayload = {
+          success: true,
+          message: await getApiMessage('products.update.success'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+
+      // update documents
+      const updatedProductAttributeResult = await productSummariesCollection.findOneAndUpdate(
+        {
+          _id: summary._id,
+        },
+        {
+          $set: {
+            attributeIds: updatedSummary.attributeIds,
+            attributes: updatedSummary.attributes.map((productAttribute) => {
+              return {
+                _id: productAttribute._id,
+                attributeId: productAttribute.attributeId,
+                filterSlugs: productAttribute.filterSlugs,
+                optionIds: productAttribute.optionIds,
+                readableValueI18n: productAttribute.readableValueI18n,
+                number: productAttribute.number,
+                textI18n: productAttribute.textI18n,
+              };
+            }),
+          },
+        },
+      );
+      if (!updatedProductAttributeResult.ok) {
+        mutationPayload = {
+          success: false,
+          message: await getApiMessage('products.update.error'),
+        };
+        await session.abortTransaction();
+        return;
+      }
+      await productFacetsCollection.findOneAndUpdate(
+        {
+          _id: summary._id,
+        },
+        {
+          $set: {
+            attributeIds: updatedSummary.attributeIds,
+          },
+        },
+      );
+
+      mutationPayload = {
+        success: true,
+        message: await getApiMessage('products.update.success'),
+        payload: summary,
+      };
+    });
+    return mutationPayload;
+  } catch (e) {
+    console.log('updateEventTextAttribute error', e);
+    return {
+      success: false,
+      message: getResolverErrorMessage(e),
+    };
+  } finally {
+    await session.endSession();
+  }
+}

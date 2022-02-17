bcbf94869e4de03fde04b73bb28c17adc3761ff3
sixnames <slava.kirshman@icloud.com>
2022-02-17 14:32:37 +0200

    events.spec.ts added

--- cypress/e2e/companies/events.spec.ts
+++ cypress/e2e/companies/events.spec.ts
@@ -1,50 +1,53 @@
+import { fixtureIds } from 'cypress/fixtures/fixtureIds';
 import { getProjectLinks } from 'lib/links/getProjectLinks';
 
-describe('Rubric products', () => {
-  const links = getProjectLinks();
+describe('Event rubric events', () => {
+  const links = getProjectLinks({
+    companyId: fixtureIds.companyA,
+  });
   beforeEach(() => {
-    cy.testAuth(links.cms.rubrics.url);
+    cy.testAuth(links.cms.companies.companyId.events.url);
   });
 
-  it('Should CRUD rubric product', () => {
-    const mainRubricName = 'Вино';
+  it('Should CRUD rubric events', () => {
+    const mainRubricName = 'Лекции';
     const newProductName = 'newProductName';
     const newProductDescription = 'newProductDescription';
     const updatedProductName = 'updatedProductName';
     const updatedProductDescription = 'updatedProductDescription';
 
-    // Should add product to rubric
+    // Should add event to rubric
     cy.getByCy(`${mainRubricName}-update`).click();
-    cy.getByCy('rubric-products-list').should('exist');
-    cy.getByCy(`create-rubric-product`).click();
-    cy.getByCy('create-new-product-modal').should('exist');
+    cy.getByCy('rubric-events-list').should('exist');
+    cy.getByCy(`create-rubric-event`).click();
+    cy.getByCy('create-new-event-modal').should('exist');
     cy.getByCy('nameI18n-ru').type(newProductName);
     cy.getByCy('originalName').type(newProductName);
     cy.getByCy('descriptionI18n-ru').type(newProductDescription);
-    cy.getByCy(`submit-new-product`).click();
+    cy.getByCy(`submit-new-event`).click();
     cy.wait(1500);
-    cy.getByCy('product-details').should('exist');
+    cy.getByCy('event-details').should('exist');
 
-    // Should update rubric product
+    // Should update rubric event
     cy.getByCy('nameI18n-ru').clear().type(updatedProductName);
     cy.getByCy('originalName').clear().type(updatedProductName);
     cy.getByCy('descriptionI18n-ru').clear().type(updatedProductDescription);
-    cy.getByCy(`submit-product`).click();
+    cy.getByCy(`submit-event`).click();
     cy.wait(1500);
-    cy.getByCy(`${updatedProductName}-product-title`).should('exist');
+    cy.getByCy(`${updatedProductName}-event-title`).should('exist');
 
-    // Should delete product from rubric
+    // Should delete event from rubric
     cy.visit(links.cms.rubrics.url);
     cy.getByCy(`${mainRubricName}-update`).click();
-    cy.getByCy('rubric-products-list').should('exist');
-    cy.getByCy('products-search-input').clear().type(updatedProductName);
-    cy.getByCy('products-search-submit').click();
-    cy.wait(1500);
+    cy.getByCy('rubric-events-list').should('exist');
+    // cy.getByCy('events-search-input').clear().type(updatedProductName);
+    // cy.getByCy('events-search-submit').click();
+    // cy.wait(1500);
     cy.getByCy(`${updatedProductName}-delete`).click();
     cy.getByCy('confirm').click();
     cy.wait(1500);
-    cy.getByCy('products-search-input').clear().type(updatedProductName);
-    cy.getByCy('products-search-submit').click();
+    // cy.getByCy('events-search-input').clear().type(updatedProductName);
+    // cy.getByCy('events-search-submit').click();
     cy.getByCy(`${updatedProductName}-delete`).should('not.exist');
   });
 });
